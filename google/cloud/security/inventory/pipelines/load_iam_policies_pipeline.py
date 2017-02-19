dc40f75a6e81b438f6349b6a7bba66eaffc482c3
Carise F <carise@users.noreply.github.com>
2017-02-18 20:53:48 -0800

    Update load_iam_policies_pipeline.py

--- google/cloud/security/inventory/pipelines/load_iam_policies_pipeline.py
+++ google/cloud/security/inventory/pipelines/load_iam_policies_pipeline.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Pipeline to load IAM policies data into Cloud Inventory."""
+"""Pipeline to load IAM policies data into Inventory."""
 
 import json
 
