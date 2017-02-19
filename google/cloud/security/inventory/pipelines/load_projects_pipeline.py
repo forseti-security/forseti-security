f31b3c2bbfbbfa60e57f4828305c74c43813bc1e
Carise F <carise@users.noreply.github.com>
2017-02-18 20:54:03 -0800

    Update load_projects_pipeline.py

--- google/cloud/security/inventory/pipelines/load_projects_pipeline.py
+++ google/cloud/security/inventory/pipelines/load_projects_pipeline.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Pipeline to load projects data into Cloud Inventory."""
+"""Pipeline to load projects data into Inventory."""
 
 import logging
 import os
