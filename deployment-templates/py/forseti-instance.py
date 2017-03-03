f057725f89da356f42102963b4dd0407d2bfcff5
Carise Fernandez <carise@google.com>
2017-03-03 12:21:59 -0800

    inventory changes

--- deployment-templates/py/forseti-instance.py
+++ deployment-templates/py/forseti-instance.py
@@ -24,6 +24,7 @@ def GenerateConfig(context):
     SCANNER_BUCKET = context.properties['scanner-bucket']
     DATABASE_NAME = context.properties['database-name']
 
+    SERVICE_ACCOUNT_SCOPES =  context.properties['service-account-scopes']
     SENDGRID_API_KEY = context.properties['sendgrid-api-key']
     EMAIL_SENDER = context.properties['email-sender']
     EMAIL_RECIPIENT = context.properties['email-recipient']
@@ -65,7 +66,7 @@ def GenerateConfig(context):
             }],
             'serviceAccounts': [{
                 'email': context.properties['service-account'],
-                'scopes': ['https://www.googleapis.com/auth/cloud-platform']
+                'scopes': SERVICE_ACCOUNT_SCOPES,
             }],
             'metadata': {
                 'items': [{
@@ -136,7 +137,7 @@ python setup.py install
 read -d '' RUN_FORSETI << EOF
 #!/bin/bash
 /usr/local/bin/forseti_inventory --organization_id {} --db_name {} --sendgrid_api_key {} --email_sender {} --email_recipient {}
-/usr/local/bin/forseti_scanner --rules {} --db_name {} --output_path {} --organization_id {} --sendgrid_api_key {} --email_sender {} --email_recipient {}
+/usr/local/bin/forseti_scanner --rules {} --output_path {} --organization_id {} --db_name {} --sendgrid_api_key {} --email_sender {} --email_recipient {}
 
 EOF
 echo "$RUN_FORSETI" > $USER_HOME/run_forseti.sh
@@ -168,9 +169,9 @@ chmod +x $USER_HOME/run_forseti.sh
 
            # - forseti_scanner
            'gs://{}/rules/rules.yaml'.format(SCANNER_BUCKET),
-           DATABASE_NAME,
            'gs://{}/scanner_violations'.format(SCANNER_BUCKET),
            context.properties['organization-id'],
+           DATABASE_NAME,
            SENDGRID_API_KEY,
            EMAIL_SENDER,
            EMAIL_RECIPIENT,
