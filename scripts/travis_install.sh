0a3871693a6d76fe069cef12fb02eeb72cfb0929
mirons-google <mirons-google@users.noreply.github.com>
2017-03-11 20:33:24 -0600

    Travis CI Integration (#114)
    * Adding a Travis CI yaml.
    
    * Making changes.
    
    * Making changes.
    
    * Further changes.
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Aadding protobuf installation script.
    
    * Updating .travis.yml
    
    * Updating install_protobuf.sh.
    
    * Updating install script and travis.
    
    * Updating install script.
    
    * Updating travis yml
    
    * Updates.
    
    * Updating travis with env variable.
    
    * Updating travis.yml
    
    * Updating travis.yml.
    
    * Updating travis.
    
    * Updating travis yml
    
    * Updating travis yml
    
    * Updating .travis.yml
    
    * Updating travis.
    
    * Updating travis.
    
    * Updating travis
    
    * updating travis
    
    * updating travis
    
    * updating travis
    
    * updating travis
    
    * Working travis YAML and accompanying dependency installation script.
    
    * Fixing YAML syntax issue.
    
    * Fixing broken env variables.
    
    * Tweaking script and yaml.
    
    * Re-adding PROTOC env variable.
    
    * In the case that we use cache, still copy over protoc to bin.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Updates.
    
    * Preparing for a PR.
    
    * Update README.md

---
+++ scripts/travis_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A script to perform the download and installation of protobuf.
+
+set -e
+
+echo "Downloading protoc."
+mkdir -p $PROTOC_DOWNLOAD_PATH
+cd $PROTOC_DOWNLOAD_PATH
+wget https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
+
+echo "Installing protoc."
+unzip protoc-3.2.0-linux-x86_64.zip
+sudo cp /tmp/protoc/bin/protoc $PROTOC_PATH
+sudo chmod 755 $PROTOC
