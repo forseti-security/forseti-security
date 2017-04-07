33f3fb9819beaa94bec03172373045c870eeab72
Henry Chang <henryc@google.com>
2017-04-06 21:28:23 -0700

    fix lint

--- tests/inventory/pipelines/test_data/fake_projects.py
+++ tests/inventory/pipelines/test_data/fake_projects.py
@@ -37,7 +37,7 @@ FAKE_PROJECTS = [
         },
         'projectId': 'project2',
         'projectNumber': '94226340476',
-        'lifecycleState': 'DELETE_REQUESTED',
+        'lifecycleState': 'ACTIVE',
         'createTime': '2016-11-13T05:32:10.930Z'
       },
       {
