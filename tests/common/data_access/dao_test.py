fc1b9ef017fdc98e8fa09a947edc0c51c0088a83
Carise Fernandez <carise@google.com>
2017-04-14 17:55:50 -0700

    More unit tests

--- tests/common/data_access/dao_test.py
+++ tests/common/data_access/dao_test.py
@@ -19,8 +19,6 @@ import json
 from google.apputils import basetest
 import mock
 
-from MySQLdb import DataError
-
 from google.cloud.security.common.data_access import _db_connector
 from google.cloud.security.common.data_access import errors
 from google.cloud.security.common.data_access import dao
@@ -83,7 +81,7 @@ class DaoTest(basetest.TestCase):
         Expect:
             * cursor() is called.
             * cursor.execute() is called.
-            * cursor.fetchall() is called.
+            * cursor.fetchone() is called.
         """
         conn_mock = mock.MagicMock()
         cursor_mock = mock.MagicMock()
@@ -106,6 +104,69 @@ class DaoTest(basetest.TestCase):
         cursor_mock.execute.assert_called_once_with(
             select_data.LATEST_SNAPSHOT_TIMESTAMP + filter_clause, statuses)
         self.assertEqual(expected_db_row[0], actual)
+
+    def test_get_latest_snapshot_timestamp_invalid_data_ok(self):
+        """Test create_snapshot_table.
+
+        Setup:
+            Create magic mocks for:
+              * conn
+              * cursor
+              * fetch
+
+        Expect:
+            * cursor() is called.
+            * cursor.execute() is called.
+            * cursor.fetchone() is called.
+        """
+        conn_mock = mock.MagicMock()
+        cursor_mock = mock.MagicMock()
+        fetch_mock = mock.MagicMock()
+
+        expected_db_row = ['123456']
+
+        self.dao.conn = conn_mock
+        self.dao.conn.cursor.return_value = cursor_mock
+        cursor_mock.fetchone = fetch_mock
+        fetch_mock.return_value = ['123456']
+
+        expected_statuses = ('SUCCESS',)
+        filter_clause = dao.SNAPSHOT_FILTER_CLAUSE.format('%s')
+
+        actual = self.dao.get_latest_snapshot_timestamp('asdfasdf')
+
+        conn_mock.cursor.assert_called_once_with()
+        fetch_mock.assert_called_once_with()
+        cursor_mock.execute.assert_called_once_with(
+            select_data.LATEST_SNAPSHOT_TIMESTAMP + filter_clause,
+            expected_statuses)
+        self.assertEqual(expected_db_row[0], actual)
+
+    def test_get_latest_snapshot_timestamp_raises_error_no_results(self):
+        """Test create_snapshot_table() raises error on no results.
+
+        Setup:
+            Create magic mocks for:
+              * conn
+              * cursor
+
+        Expect:
+            Raise MySQLError for no results.
+        """
+        conn_mock = mock.MagicMock()
+        cursor_mock = mock.MagicMock()
+        exec_mock = mock.MagicMock()
+
+        self.dao.conn = conn_mock
+        self.dao.conn.cursor.return_value = cursor_mock
+        cursor_mock.execute = exec_mock
+        exec_mock.side_effect = errors.NoResultsError
+
+        expected_statuses = ('SUCCESS',)
+        filter_clause = dao.SNAPSHOT_FILTER_CLAUSE.format('%s')
+
+        with self.assertRaises(errors.MySQLError):
+            self.dao.get_latest_snapshot_timestamp('asdfasdf')
 
 
 if __name__ == '__main__':
