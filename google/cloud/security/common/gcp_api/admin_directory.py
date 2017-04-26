02ca7ac32b7227630e21c79443b9e5ce87070480
Gianluca Brindisi <gianluca@spotify.com>
2017-04-26 18:02:26 +0200

    Added maxResult param to list() call and max_results_admin_api FLAG to store the value

--- google/cloud/security/common/gcp_api/admin_directory.py
+++ google/cloud/security/common/gcp_api/admin_directory.py
@@ -36,6 +36,8 @@ flags.DEFINE_string('groups_service_account_key_file', None,
                     'runnning locally.')
 flags.DEFINE_integer('max_admin_api_calls_per_day', 150000,
                      'Admin SDK queries per day.')
+flags.DEFINE_integer('max_results_admin_api', 500,
++                     'maxResult param for the Admin SDK list() method')
 
 
 class AdminDirectoryClient(_base_client.BaseClient):
@@ -94,7 +96,8 @@ class AdminDirectoryClient(_base_client.BaseClient):
             api_errors.ApiExecutionError
         """
         members_stub = self.service.members()
-        request = members_stub.list(groupKey=group_key)
+        request = members_stub.list(groupKey=group_key,
+                                    maxResults=FLAGS.max_results_admin_api)
         results_by_member = []
 
         # TODO: Investigate yielding results to handle large group lists.
