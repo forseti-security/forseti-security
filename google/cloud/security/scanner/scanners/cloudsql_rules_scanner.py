449887ade559ae0f6170ced0e449f62d3cee59cb
thenenadx <thenenadx@users.noreply.github.com>
2017-05-22 18:34:46 +0200

    Cloudsqlrulesengine (#290)
    * Inventory & Scanner should support scanning +
    policies on Google Cloud Storage buckets (#59)
    This commit adds support to inventory for GCS buckets collection
    
    * Initial buckets acls commit. Not finished yet
    
    * Additional files for the initial commit
    
    * Some small style fixes
    
    * Update create_tables.py
    
    A small edit in the TODO comment to try and rerun the travis build.
    
    * Update create_tables.py
    
    * Fix lynt errors
    
    * Improved bucket ACL pipeline, still wip
    
    * Initialized the bucket dao
    
    * Update load_projects_buckets_pipeline.py
    
    Fix lint.
    
    * Update storage.py
    
    Fix lint.
    
    * Support for bucket acls collection
    
    * Small fixes
    
    * Yet another style fix
    
    * Remove new line
    
    * Tests for bucket apis and bucket acl apis
    
    * Style fix
    
    * Style fixes
    
    * Implements Travis fixes
    
    * Travis fixes
    
    * Fix Travis errors
    
    * Update csv_writer.py
    
    * Update bucket_dao.py
    
    * Update bucket_dao.py
    
    * Improve inheritance in bucket_dao
    
    * Make Travis a happy camper
    
    * Fix for tests after the improvement for inheritance
    
    * Update bucket_dao.py
    
    * Update bucket_dao.py
    
    * Update csv_writer.py
    
    * Update storage.py
    
    * Update load_projects_buckets_acls_pipeline.py
    
    * Update csv_writer.py
    
    * Update bucket_dao_test.py
    
    * Improvement on loading rules engines more dynamically.
    This requires that rules engines have a build_rule_book() function.
    
    * Style fixes
    
    * More fixes for Travis
    
    * Inventory support for CloudSQL
    
    * Cloudsql scanner
    
    * Fixed conflicts
    
    * Style fixes
    
    * Typo fixes
    
    * Bug fixes
    
    * fix lint
    
    * fix lint
    
    * disable invalid-name check
    
    * Fix for tests
    
    * Clean up comments

---
+++ google/cloud/security/scanner/scanners/cloudsql_rules_scanner.py
@@ -0,0 +1,108 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Scanner for the CloudSQL acls rules engine."""
+from google.cloud.security.common.util import log_util
+from google.cloud.security.common.data_access import cloudsql_dao
+from google.cloud.security.common.data_access import project_dao
+from google.cloud.security.common.gcp_type.resource import ResourceType
+from google.cloud.security.scanner.scanners import base_scanner
+
+LOGGER = log_util.get_logger(__name__)
+
+
+class CloudSqlAclScanner(base_scanner.BaseScanner):
+    """Pipeline to CloudSQL acls data from DAO"""
+    def __init__(self, snapshot_timestamp):
+        """Initialization.
+
+        Args:
+            snapshot_timestamp: The snapshot timestamp
+        """
+        super(CloudSqlAclScanner, self).__init__(
+            snapshot_timestamp)
+        self.snapshot_timestamp = snapshot_timestamp
+
+    def _get_project_policies(self):
+        """Get projects from data source.
+        """
+        project_policies = {}
+        project_policies = (
+            project_dao.ProjectDao().get_project_policies('projects',
+                                                          self.\
+                                                          snapshot_timestamp))
+        return project_policies
+
+    def _get_cloudsql_acls(self):
+        """Get CloudSQL acls from data source.
+        """
+        cloudsql_acls = {}
+        cloudsql_acls = cloudsql_dao.CloudsqlDao().\
+                        get_cloudsql_acls('cloudsql_instances',
+                                          self.snapshot_timestamp)
+
+        return cloudsql_acls
+
+    @staticmethod
+    def _get_resource_count(project_policies, cloudsql_acls):
+        """Get resource count for org and project policies.
+
+        Args:
+            org_policies: organization policies from inventory.
+            project_policies: project policies from inventory.
+        Returns:
+            Resource count map
+        """
+        resource_counts = {
+            ResourceType.PROJECT: len(project_policies),
+            ResourceType.CLOUDSQL_ACL: len(cloudsql_acls),
+        }
+
+        return resource_counts
+
+    def run(self):
+        """Runs the data collection."""
+        cloudsql_acls_data = []
+        project_policies = {}
+        cloudsql_acls = self._get_cloudsql_acls()
+        cloudsql_acls_data.append(cloudsql_acls.iteritems())
+        cloudsql_acls_data.append(project_policies.iteritems())
+
+        resource_counts = self._get_resource_count(project_policies,
+                                                   cloudsql_acls)
+
+        return cloudsql_acls_data, resource_counts
+
+    # pylint: disable=arguments-differ
+    def find_violations(self, cloudsql_data, rules_engine):
+        """Find violations in the policies.
+
+        Args:
+            cloudsql_data: CloudSQL data to find violations in
+            rules_engine: The rules engine to run.
+
+        Returns:
+            A list of violations
+        """
+
+        all_violations = []
+        LOGGER.info('Finding CloudSQL acl violations...')
+
+        for (cloudsql, cloudsql_acl) in cloudsql_data:
+            LOGGER.debug('%s => %s', cloudsql, cloudsql_acl)
+            violations = rules_engine.find_policy_violations(
+                cloudsql_acl)
+            LOGGER.debug(violations)
+            all_violations.extend(violations)
+        return all_violations
