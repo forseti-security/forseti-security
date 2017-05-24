3fe411a1caf04c899ff86263716014c8231c67a4
Felix Matenaar <fmatenaar@google.com>
2017-05-24 13:39:10 -0700

    Dockerization test

--- tests/unittest_utils.py
+++ tests/unittest_utils.py
@@ -17,6 +17,7 @@
 import json
 import os
 import unittest
+import collections
 
 class ForsetiTestCase(unittest.TestCase):
     """Forseti base class for tests."""
@@ -29,6 +30,82 @@ class ForsetiTestCase(unittest.TestCase):
         """
         if not actual.startswith(expected_start):
             self.fail('%r does not start with %r' % (actual, expected_start))
+
+    def assertSameStructure(self, a, b, aname='a', bname='b', msg=None):
+        """Asserts that two values contain the same structural content.
+        The two arguments should be data trees consisting of trees of dicts and
+        lists. They will be deeply compared by walking into the contents of dicts
+        and lists; other items will be compared using the == operator.
+        If the two structures differ in content, the failure message will indicate
+        the location within the structures where the first difference is found.
+        This may be helpful when comparing large structures.
+        Args:
+            a: The first structure to compare.
+            b: The second structure to compare.
+            aname: Variable name to use for the first structure in assertion messages.
+            bname: Variable name to use for the second structure.
+            msg: Additional text to include in the failure message.
+        """
+
+        # Accumulate all the problems found so we can report all of them at once
+        # rather than just stopping at the first
+        problems = []
+
+        _WalkStructureForProblems(a, b, aname, bname, problems)
+
+        # Avoid spamming the user toooo much
+        max_problems_to_show = self.maxDiff // 80
+        if len(problems) > max_problems_to_show:
+            problems = problems[0:max_problems_to_show-1] + ['...']
+
+        if problems:
+            failure_message = '; '.join(problems)
+            if msg:
+                failure_message += (': ' + msg)
+            self.fail(failure_message)
+
+_INT_TYPES = (int, long)  # Sadly there is no types.IntTypes defined for us.
+
+
+def _WalkStructureForProblems(a, b, aname, bname, problem_list):
+  """The recursive comparison behind assertSameStructure."""
+  if type(a) != type(b) and not (
+      isinstance(a, _INT_TYPES) and isinstance(b, _INT_TYPES)):
+    # We do not distinguish between int and long types as 99.99% of Python 2
+    # code should never care.  They collapse into a single type in Python 3.
+    problem_list.append('%s is a %r but %s is a %r' %
+                        (aname, type(a), bname, type(b)))
+    # If they have different types there's no point continuing
+    return
+
+  if isinstance(a, collections.Mapping):
+    for k in a:
+      if k in b:
+        _WalkStructureForProblems(a[k], b[k],
+                                  '%s[%r]' % (aname, k), '%s[%r]' % (bname, k),
+                                  problem_list)
+      else:
+        problem_list.append('%s has [%r] but %s does not' % (aname, k, bname))
+    for k in b:
+      if k not in a:
+        problem_list.append('%s lacks [%r] but %s has it' % (aname, k, bname))
+
+  # Strings are Sequences but we'll just do those with regular !=
+  elif isinstance(a, collections.Sequence) and not isinstance(a, basestring):
+    minlen = min(len(a), len(b))
+    for i in xrange(minlen):
+      _WalkStructureForProblems(a[i], b[i],
+                                '%s[%d]' % (aname, i), '%s[%d]' % (bname, i),
+                                problem_list)
+    for i in xrange(minlen, len(a)):
+      problem_list.append('%s has [%i] but %s does not' % (aname, i, bname))
+    for i in xrange(minlen, len(b)):
+      problem_list.append('%s lacks [%i] but %s has it' % (aname, i, bname))
+
+  else:
+    if a != b:
+      problem_list.append('%s is %r but %s is %r' % (aname, a, bname, b))
+
 
 def get_datafile_path(start_loc, filename):
     """Get the path for a data file."""
