a2d40665a8a546a482ca83b78d8aa0a65498b762
Henry Chang <henryc@google.com>
2017-06-01 14:51:39 -0700

    tweak

--- google/cloud/security/notifier/pipelines/email_inventory_snapshot_summary_pipeline.py
+++ google/cloud/security/notifier/pipelines/email_inventory_snapshot_summary_pipeline.py
@@ -1,4 +1,3 @@
-
 # Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Email pipeline to notify that inventory snapshots have been completed."""
 
 # TODO: Investigate improving so we can avoid the pylint disable.
