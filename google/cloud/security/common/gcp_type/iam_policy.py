677e172c9f65b14827bfc81619420104675aad8b
mikeirons <mirons@google.com>
2017-06-13 15:29:20 -0500

    Test fixes.

--- google/cloud/security/common/gcp_type/iam_policy.py
+++ google/cloud/security/common/gcp_type/iam_policy.py
@@ -99,7 +99,7 @@ class IamPolicy(object):
         Returns:
             True if bindings are empty; False otherwise.
         """
-        return bool(self.bindings)
+        return not bool(self.bindings)
 
 
 class IamPolicyBinding(object):
