5507eb972ef0c5b00d0a4bcb6915ba11a5647e29
mirons <mirons-google@users.noreply.github.com>
2017-06-13 18:31:52 -0500

    Enabling the pylint plugin pylint.extensions.bad_builtin (#348)
    * Improvements.
    
    * Enabling the pylint.extensions.check_elif plugin.
    
    * Fix.
    
    * Tweaks.
    
    * Enabling pylint plugin pylint.extensions.comparetozero
    
    * Test fixes.
    
    * Adding a pylint disable and TODO to revisit in iam_rules_engine.
    
    * Enabling pylint plugin pylint.extensions.bad_builtin.
    
    * Tweak.
    
    * Repalce list comprehension with for loop.
    
    * Moving pylint configurations from the command line to rcfile for consistency across users (#349)
    
    * pylintrc improvements
    
    * Increasing jobs to 8.
    
    * Enabling the pylint plugin pylint.extensions.docstyle (#350)
    
    * pylintrc improvements
    
    * Increasing jobs to 8.
    
    * New plugin
    
    * Enabling the pylint plugin pylint.extensions.docstyle

--- google/cloud/security/common/util/log_util.py
+++ google/cloud/security/common/util/log_util.py
@@ -54,7 +54,8 @@ def _map_logger(func):
     Args:
         func: Function to call on every logger.
     """
-    map(func, LOGGERS.itervalues())
+    for logger in LOGGERS.itervalues():
+        func(logger)
 
 def set_logger_level(level):
     """Modify log level of existing loggers as well as the default
@@ -63,7 +64,6 @@ def set_logger_level(level):
     Args:
         level: The log level to set the loggers to.
     """
-
     # pylint: disable=global-statement
     global LOGLEVEL
     LOGLEVEL = level
