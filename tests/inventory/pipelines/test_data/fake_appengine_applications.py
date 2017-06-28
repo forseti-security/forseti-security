bdac005e0b97458b88ab953820f7f965241a3f85
Liang Zhang <leoliangzhang@gmail.com>
2017-06-28 16:28:33 -0700

    Fix format issues.

--- tests/inventory/pipelines/test_data/fake_appengine_applications.py
+++ tests/inventory/pipelines/test_data/fake_appengine_applications.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
@@ -13,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-'''Test applications data.'''
+""" applications data."""
 
 from google.cloud.security.common.util import parser
 
