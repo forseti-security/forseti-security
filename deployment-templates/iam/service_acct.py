afd5047b758e2acd4bcda4d6d85bc30fc51d7b4a
Henry Chang <henryc@google.com>
2017-08-25 16:03:34 -0700

    Tweak copyright. (#595)
    * tweak copyright
    
    * tweak copyright

--- deployment-templates/iam/service_acct.py
+++ deployment-templates/iam/service_acct.py
@@ -1,4 +1,4 @@
-# Copyright 2017 The Forseti Security Authors. All rights reserved. All rights reserved.
+# Copyright 2017 The Forseti Security Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
