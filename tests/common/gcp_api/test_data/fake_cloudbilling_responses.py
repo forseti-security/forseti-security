7e814ba295682a1c7fef8e8494b67d17245a2b09
Andrew Hoying <ahoying@google.com>
2017-08-30 13:28:47 -0700

    Add a cloudbilling API repository and client.
    Waiting for the new inventory database design to create the dao and
    pipeline to pull data.

---
+++ tests/common/gcp_api/test_data/fake_cloudbilling_responses.py
@@ -0,0 +1,55 @@
+# Copyright 2017 The Forseti Security Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test data for Compute GCP api responses."""
+
+FAKE_PROJECT_ID = "project1"
+
+GET_PROJECTS_BILLING_INFO = """
+{
+ "name": "projects/projects/billingInfo",
+ "projectId": "project1",
+ "billingAccountName": "billingAccounts/000000-111111-222222",
+ "billingEnabled": true
+}
+"""
+
+GET_PROJECTS_BILLING_INFO_NOT_ENABLED = """
+{
+ "name": "projects/projects/billingInfo",
+ "projectId": "project1"
+}
+"""
+
+# Errors
+
+PROJECT_NOT_FOUND = """
+{
+ "error": {
+  "code": 404,
+  "message": "Requested entity was not found.",
+  "status": "NOT_FOUND"
+ }
+}
+"""
+
+PERMISSION_DENIED = """
+{
+ "error": {
+  "code": 403,
+  "message": "The caller does not have permission",
+  "status": "PERMISSION_DENIED"
+ }
+}
+"""
