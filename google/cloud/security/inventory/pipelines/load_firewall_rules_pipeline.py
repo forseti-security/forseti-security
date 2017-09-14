a82e58c731b717caa88d325aceaa9c420088e186
Carise F <carise@users.noreply.github.com>
2017-09-14 09:45:56 -0700

    Revert "Add targetServiceAccount to firewall rules inventory (#638)"
    This reverts commit db1c5a5bc1db05a55d31324847ffee44091d9299.

--- google/cloud/security/inventory/pipelines/load_firewall_rules_pipeline.py
+++ google/cloud/security/inventory/pipelines/load_firewall_rules_pipeline.py
@@ -70,9 +70,6 @@ class LoadFirewallRulesPipeline(base_pipeline.BasePipeline):
                        'firewall_rule_target_tags':
                            parser.json_stringify(
                                firewall_rule.get('targetTags')),
-                       'firewall_rule_target_service_accounts':
-                            parser.json_stringify(
-                                firewall_rule.get('targetServiceAccounts')),
                        'firewall_rule_allowed':
                            parser.json_stringify(
                                firewall_rule.get('allowed')),
