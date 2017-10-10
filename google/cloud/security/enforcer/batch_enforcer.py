1d725d838ed771e2eca244273669e7d013077361
Andrew Hoying <ahoying@google.com>
2017-10-10 13:30:02 -0700

    Change grouping of imports.

--- google/cloud/security/enforcer/batch_enforcer.py
+++ google/cloud/security/enforcer/batch_enforcer.py
@@ -17,8 +17,8 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 import threading
+
 import concurrent.futures
 from google.apputils import datelib
 
