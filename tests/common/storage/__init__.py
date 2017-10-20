76ab1236e02bc53ef2955c00a94d22b987f152c4
Henry Chang <henryc@google.com>
2017-10-19 17:15:47 -0700

    move tests

--- tests/common/storage/__init__.py
+++ tests/common/storage/__init__.py
@@ -1,4 +1,3 @@
-#
 # Copyright 2017 The Forseti Security Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for IAM services."""
+"""Storage tests."""
