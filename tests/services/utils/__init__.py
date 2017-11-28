840a9709d27a5f3db68378e8b42dafba52022f25
Andrew Hoying <ahoying@google.com>
2017-11-28 12:14:24 -0800

    Update init comments.

--- tests/services/utils/__init__.py
+++ tests/services/utils/__init__.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Utils for IAM Explain testing."""
+"""Utils for Forseti services testing."""
