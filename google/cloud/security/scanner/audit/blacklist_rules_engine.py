17dc256204cecbb632d77bda77442b1f32a4ccce
Anton Sapozhnikov <asapozhnikov@spotify.com>
2018-01-15 14:59:21 +0100

    init commit

---
+++ google/cloud/security/scanner/audit/blacklist_rules_engine.py
@@ -0,0 +1,265 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Rules engine for Blacklist of IP addresses."""
+import itertools
+import re
+import urllib2
+import struct
+import socket
+
+
+from collections import namedtuple
+
+
+from google.cloud.security.common.util import log_util
+from google.cloud.security.scanner.audit import base_rules_engine as bre
+
+
+LOGGER = log_util.get_logger(__name__)
+
+
+class BlacklistRulesEngine(bre.BaseRulesEngine):
+    """Rules engine for BlacklistRules."""
+
+    def __init__(self, rules_file_path, snapshot_timestamp=None):
+        """Initialize.
+
+        Args:
+            rules_file_path (str): file location of rules
+            snapshot_timestamp (str): timestamp for database.
+        """
+        super(BlacklistRulesEngine,
+              self).__init__(rules_file_path=rules_file_path)
+        self.rule_book = None
+
+    def build_rule_book(self, global_configs=None):
+        """Build BlacklistRuleBook from rules definition file.
+
+        Args:
+            global_configs (dict): Global Configs
+        """
+        self.rule_book = BlacklistRuleBook(
+            self._load_rule_definitions())
+
+    def find_policy_violations(self, instance_network_interface,
+                               force_rebuild=False):
+        """Determine whether the networks violates rules.
+
+        Args:
+            instance_network_interface (list): list of
+                instance_network_interface
+            force_rebuild (bool): set to false to not force a rebuiid
+
+        Return:
+            list: iterator of all violations
+        """
+        violations = itertools.chain()
+        if self.rule_book is None or force_rebuild:
+            self.build_rule_book()
+        resource_rules = self.rule_book.get_resource_rules()
+
+        for rule in resource_rules:
+            violations = itertools.chain(violations,
+                                         rule.find_violations(
+                                             instance_network_interface))
+        return violations
+
+    def add_rules(self, rules):
+        """Add rules to the rule book.
+
+        Args:
+            rules (dicts): rule definitions
+        """
+        if self.rule_book is not None:
+            self.rule_book.add_rules(rules)
+
+class BlacklistRuleBook(bre.BaseRuleBook):
+    """The RuleBook for networks resources."""
+
+    def __init__(self,
+                 rule_defs=None):
+        """Initialize.
+
+        Args:
+            rule_defs (dict): The parsed dictionary of rules from the YAML
+                definition file.
+        """
+        super(BlacklistRuleBook, self).__init__()
+        self.resource_rules_map = {}
+        if not rule_defs:
+            self.rule_defs = {}
+        else:
+            self.rule_defs = rule_defs
+            self.add_rules(rule_defs)
+
+    def add_rules(self, rule_defs):
+        """Add rules to the rule book.
+
+        Args:
+            rule_defs (dict): rules definitions
+        """
+        for (i, rule) in enumerate(rule_defs.get('rules', [])):
+            self.add_rule(rule, i)
+
+    def add_rule(self, rule_def, rule_index):
+        """Add a rule to the rule book.
+        Args:
+            rule_def (dict): A dictionary containing rule definition
+                properties.
+            rule_index (int): The index of the rule from the rule definitions.
+                Assigned automatically when the rule book is built.
+        """
+
+        ip_file_url = rule_def.get('threats_url')
+
+        ips, nets = self.get_and_parse_threat_url(ip_file_url)
+
+        rule_def_resource = {
+            "ips_list": ips,
+            "nets_list": nets
+        }
+
+        rule = Rule(rule_name=rule_def.get('name'),
+                    rule_index=rule_index,
+                    rules=rule_def_resource)
+
+        resource_rules = self.resource_rules_map.get(rule_index)
+
+        if not resource_rules:
+            self.resource_rules_map[rule_index] = rule
+
+
+    def get_resource_rules(self):
+        """Get all the resource rules for (resource, RuleAppliesTo.*).
+
+        Returns:
+           list:  A list of ResourceRules.
+        """
+        resource_rules = []
+
+        for resource_rule in self.resource_rules_map:
+            resource_rules.append(self.resource_rules_map[resource_rule])
+
+        return resource_rules
+
+    @staticmethod
+    def get_and_parse_threat_url(url):
+        """Download blacklist from provided url and parse it
+        to ips and net blocks
+
+        Args:
+            url (str): url to download blacklist from
+
+        Returns:
+            lists: first one is IP addresses,
+            second one is network blocks
+
+        """
+        data = urllib2.urlopen(url).read()
+        ips = re.findall(r'[0-9]+(?:\.[0-9]+){3}', data)
+        nets = re.findall(
+            r'(?<!\d\.)(?<!\d)(?:\d{1,3}\.){3}\d{1,3}/\d{1,2}(?!\d|(?:\.\d))',
+            data)
+        return ips, nets
+
+class Rule(object):
+    """The rules class for instance_network_interface."""
+
+    def __init__(self, rule_name, rule_index, rules):
+        """Initialize.
+
+        Args:
+            rule_name (str): Name of the loaded rule
+            rule_index (int): The index of the rule from the  definitions
+            rules (dict): The resources associated with the rules like
+                the whitelist
+        """
+        self.rule_name = rule_name
+        self.rule_index = rule_index
+        self.rules = rules
+
+    @staticmethod
+    def address_in_network(ipaddr, net):
+        """ Checks if ip address is in net
+
+        Args:
+            ipaddr (str): IP address to check
+            net (str): network to check
+
+        Returns:
+            bool: True if ipaddr in net
+        """
+        ipaddrb = struct.unpack("!I", socket.inet_aton(ipaddr))[0]
+        netstr, bits = net.split('/')
+        netaddr = struct.unpack("!I", socket.inet_aton(netstr))[0]
+        mask = (0xffffffff << (32 - int(bits))) & 0xffffffff
+        return (ipaddrb & mask) == (netaddr & mask)
+
+    def check_if_blacklisted(self, ipaddr):
+        """ Checks if ip address is in a blacklist
+
+        Args:
+            ipaddr (str): IP address to check
+
+        Returns:
+            bool: True if ipaddr is blacklisted
+        """
+        if ipaddr in self.rules['ips_list']:
+            return True
+        for ip_network in self.rules['nets_list']:
+            if self.address_in_network(ipaddr, ip_network):
+                return True
+        return False
+
+    def find_violations(self, instance_network_interface):
+        """Raise violation if the IP is not in the whitelist.
+
+        Args:
+            instance_network_interface (InstanceNetworkInterface): object
+
+        Yields:
+            namedtuple: Returns RuleViolation named tuple
+        """
+
+        network_and_project = re.search(
+            r'compute/v1/projects/([^/]*).*networks/([^/]*)',
+            instance_network_interface.network)
+        project = network_and_project.group(1)
+        network = network_and_project.group(2)
+
+        for access_config in instance_network_interface.access_configs:
+            ipaddr = access_config['natIP']
+            if self.check_if_blacklisted(ipaddr):
+                yield self.RuleViolation(
+                    resource_type='instance',
+                    rule_name=self.rule_name,
+                    rule_index=self.rule_index,
+                    violation_type='BLACKLIST_VIOLATION',
+                    project=project,
+                    network=network,
+                    ip=access_config['natIP'])
+
+    # Rule violation.
+    # resource_type: string
+    # rule_name: string
+    # rule_index: int
+    # violation_type: BLACKLIST_VIOLATION
+    # project: string
+    # network: string
+    # ip: string
+    RuleViolation = namedtuple('RuleViolation',
+                               ['resource_type', 'rule_name',
+                                'rule_index', 'violation_type', 'project',
+                                'network', 'ip'])
---
+++ google/cloud/security/scanner/scanners/blacklist_scanner.py
@@ -0,0 +1,185 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with azthe License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Scanner for the Networks Enforcer acls rules engine."""
+
+from google.cloud.security.common.util import log_util
+from google.cloud.security.common.data_access import instance_dao
+from google.cloud.security.common.data_access import project_dao
+from google.cloud.security.common.gcp_type.resource import ResourceType
+from google.cloud.security.scanner.scanners import base_scanner
+from google.cloud.security.scanner.audit import blacklist_rules_engine
+
+
+LOGGER = log_util.get_logger(__name__)
+
+
+class BlacklistScanner(base_scanner.BaseScanner):
+    """Pipeline to network enforcer from DAO."""
+
+    def __init__(self, global_configs, scanner_configs,
+                 snapshot_timestamp, rules):
+        """Initialization.
+
+         Args:
+            global_configs (dict): Global configurations.
+            scanner_configs (dict): Scanner configurations.
+            snapshot_timestamp (str): Timestamp, formatted as YYYYMMDDTHHMMSSZ.
+            rules (str): Fully-qualified path and filename of the rules file.
+        """
+        super(BlacklistScanner, self).__init__(
+            global_configs,
+            scanner_configs,
+            snapshot_timestamp,
+            rules)
+        self.rules_engine = (
+            blacklist_rules_engine
+            .BlacklistRulesEngine(
+                rules_file_path=self.rules,
+                snapshot_timestamp=self.snapshot_timestamp))
+        self.rules_engine.build_rule_book(self.global_configs)
+
+    @staticmethod
+    def _flatten_violations(violations):
+        """Flatten RuleViolations into a dict for each RuleViolation member.
+
+        Args:
+            violations (list): The RuleViolations to flatten.
+
+        Yields:
+            dict: Iterator of RuleViolations as a dict per member.
+        """
+        for violation in violations:
+            violation_data = {}
+            violation_data['project'] = violation.project
+            violation_data['network'] = violation.network
+            violation_data['ip'] = violation.ip
+            yield {
+                'resource_id': 'instance_network_interface',
+                'resource_type': violation.resource_type,
+                'rule_index': violation.rule_index,
+                'rule_name': violation.rule_name,
+                'violation_type': violation.violation_type,
+                'violation_data': violation_data
+            }
+
+    def _output_results(self, all_violations):
+        """Output results.
+
+        Args:
+            all_violations (list): All violations
+        """
+        resource_name = 'violations'
+
+        all_violations = self._flatten_violations(all_violations)
+        self._output_results_to_db(resource_name, all_violations)
+
+    # pylint: disable=invalid-name
+    def get_instance_networks_interfaces(self):
+        """Get network info from a particular snapshot.
+
+           Returns:
+               list: A list of networks from a particular project
+
+           Raises:
+               MySQLError if a MySQL error occurs.
+        """
+
+        instances = instance_dao.InstanceDao(self.global_configs).get_instances(
+            self.snapshot_timestamp)
+        network_interfaces = []
+        for instance in instances:
+            network_interfaces += instance.create_network_interfaces()
+        return network_interfaces
+
+    @staticmethod
+    def parse_instance_network_instance(instance_object):
+        """Create a list of network interface obj.
+
+        Args:
+            instance_object (instance_object): an instance object
+
+        Returns:
+            list: a list of network interface objects
+        """
+        return instance_object.create_network_interfaces()
+
+    def _get_project_policies(self):
+        """Get projects from data source.
+
+        Returns:
+            dict: project policies
+        """
+        project_policies = {}
+        project_policies = (
+            project_dao
+            .ProjectDao()
+            .get_project_policies('projects',
+                                  self.
+                                  snapshot_timestamp))
+        return project_policies
+
+    @staticmethod
+    def _get_resource_count(project_policies, instance_network_interfaces):
+        """Get resource count for org and project policies.
+
+        Args:
+            project_policies (dict): containing the projects
+                (gcp_type.project.Project) and their iam policies (dict).
+            instance_network_interfaces (list): of network_interface objects.
+
+        Returns:
+            dict: Resource count map
+        """
+        resource_counts = {
+            ResourceType.PROJECT: len(project_policies),
+            ResourceType.INSTANCE: len(instance_network_interfaces),
+        }
+
+        return resource_counts
+
+    def _retrieve(self):
+        """Run the data collection.
+
+        Return:
+           list: instance_networks_interfaces
+        """
+        return self.get_instance_networks_interfaces()
+
+    def _find_violations(self, instance_networks_data):
+        """Find violations in the policies.
+
+            Args:
+                instance_networks_data (list): instance networks data
+                    to find violations in
+
+            Returns:
+                list: A list of violations
+        """
+        all_violations = []
+        LOGGER.info('Finding blacklisted ip addresses...')
+        for instance_network_interface in instance_networks_data:
+            LOGGER.debug('%s', instance_network_interface)
+            violations = self.rules_engine.find_policy_violations(
+                instance_network_interface)
+            LOGGER.debug(violations)
+            all_violations.extend(violations)
+        return all_violations
+
+    def run(self):
+        """Runs the data collection."""
+        instance_network_interface_data = self._retrieve()
+        all_violations = (
+            self._find_violations(instance_network_interface_data))
+        self._output_results(all_violations)
