015bbc7d49d60a6f4b2da9a65339ddaea8e05887
Henry Chang <henryc@google.com>
2018-01-18 15:27:39 -0800

    more test tweak

--- tests/scanner/audit/iam_rules_engine_test.py
+++ tests/scanner/audit/iam_rules_engine_test.py
@@ -22,8 +22,6 @@ import yaml
 import unittest
 
 from tests.unittest_utils import ForsetiTestCase
-from google.cloud.forseti.common.data_access import _db_connector
-from google.cloud.forseti.common.data_access import org_resource_rel_dao as org_rel_dao
 from google.cloud.forseti.common.gcp_type import folder
 from google.cloud.forseti.common.gcp_type.iam_policy import IamPolicyBinding
 from google.cloud.forseti.common.gcp_type.iam_policy import IamPolicyMember
