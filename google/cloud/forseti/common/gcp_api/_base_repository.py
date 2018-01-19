8771e34d7003d1d593cd374a809ca456ce082d69
mirons-google <mirons-google@users.noreply.github.com>
2018-01-18 18:31:48 -0600

    Updates.

--- google/cloud/forseti/common/gcp_api/_base_repository.py
+++ google/cloud/forseti/common/gcp_api/_base_repository.py
@@ -15,14 +15,12 @@
 """Base GCP client which uses the discovery API."""
 import logging
 import threading
-import httplib2
-
-from oauth2client import client
-from ratelimiter import RateLimiter
 
 import googleapiclient
 from googleapiclient import discovery
-
+import httplib2
+from oauth2client import client
+from ratelimiter import RateLimiter
 from retrying import retry
 
 from google.cloud import forseti as forseti_security
