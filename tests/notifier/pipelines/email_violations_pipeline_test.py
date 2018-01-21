ffae9cb378e950e5590f1716f02e71ea3c0456b9
Muharem Hrnjadovic <altair@google.com>
2018-01-20 21:11:05 -0500

    os.environ['TMPDIR'] now happens on module level

--- tests/notifier/pipelines/email_violations_pipeline_test.py
+++ tests/notifier/pipelines/email_violations_pipeline_test.py
@@ -19,12 +19,33 @@ from datetime import datetime
 import glob
 import mock
 import os
+import random
 import shutil
-import tempfile
 import unittest
 
 from google.cloud.security.notifier.pipelines import email_violations_pipeline
 from tests.unittest_utils import ForsetiTestCase
+
+
+_ORIGINAL_TMPDIR = os.environ.get('TMPDIR')
+
+
+def setUpModule():
+    """Module level setup: the first import of `tempfile` "freezes" the value
+    of `os.environ['TMPDIR']`.
+    We thus need to perform this initialization only once and _before_ that
+    first import."""
+    os.environ['TMPDIR'] = '/tmp/forseti--{}'.format(random.randint(1, 100000))
+    os.mkdir(os.environ['TMPDIR'])
+
+
+def tearDownModule():
+    """Module level teardown."""
+    shutil.rmtree(os.environ['TMPDIR'], ignore_errors=True)
+    if _ORIGINAL_TMPDIR:
+        os.environ['TMPDIR'] = _ORIGINAL_TMPDIR
+    else:
+        del(os.environ['TMPDIR'])
 
 
 class EmailViolationsPipelineTest(ForsetiTestCase):
@@ -32,9 +53,6 @@ class EmailViolationsPipelineTest(ForsetiTestCase):
 
     def setUp(self):
         """Setup."""
-        self.original_tmpdir = os.environ.get('TMPDIR')
-        os.environ['TMPDIR'] = tempfile.mkdtemp(prefix='forseti.')
-
         fake_global_conf = {
             'db_host': 'x',
             'db_name': 'y',
@@ -47,38 +65,32 @@ class EmailViolationsPipelineTest(ForsetiTestCase):
             'sendgrid_api_key': 'foo_email_key',
         }
 
-        self.gvp = email_violations_pipeline.EmailViolationsPipeline(
+        self.gvp_args = [
             'abcd',
             datetime.strftime(datetime.utcnow(), '%Y%m%dT%H%M%SZ'),
             ['violation: abc', 'violation: def'],
             fake_global_conf,
             {},
-            fake_pipeline_conf)
-
-    def tearDown(self):
-        """Teardown."""
-        shutil.rmtree(os.environ['TMPDIR'], ignore_errors=True)
-        if self.original_tmpdir:
-            os.environ['TMPDIR'] = self.original_tmpdir
-        else:
-            del(os.environ['TMPDIR'])
+            fake_pipeline_conf]
 
     @mock.patch(
         'google.cloud.security.common.util.email_util.EmailUtil',
         autospec=True)
     def test_make_attachment_cleans_up_tempfile(self, mock_emailutil):
-        """Test that _make_attachment() leaves no temp files behind."""
-        tmp_dir = '{}/violations.*'.format(os.environ['TMPDIR'])
+        """Test that we leave no temp files behind."""
+        tmp_dir = '{}/*'.format(os.environ['TMPDIR'])
         tmp_files_before = glob.glob(tmp_dir)
-        self.gvp.mail_util = mock_emailutil
-        self.gvp.run()
+        gvp = email_violations_pipeline.EmailViolationsPipeline(*self.gvp_args)
+        gvp.mail_util = mock_emailutil
+        gvp.run()
         tmp_files_after = glob.glob(tmp_dir)
         self.assertEquals(tmp_files_before, tmp_files_after)
 
     def test_make_attachment_can_read_tempfile(self):
         """Test that _make_attachment() can read the temp file."""
         violations = '["violation: abc", "violation: def"]'
-        attachment = self.gvp._make_attachment()
+        gvp = email_violations_pipeline.EmailViolationsPipeline(*self.gvp_args)
+        attachment = gvp._make_attachment()
         decoded_content = base64.b64decode(attachment.content)
         self.assertEquals(decoded_content, violations)
 
