96846ae348824c0f82f4fd3376138bb56debeb6e
Muharem Hrnjadovic <altair@google.com>
2018-01-24 21:49:56 +0100

    fixes: group members inventory marked as success when groups failed (#997)
    * added tests for _postprocess_statuses()
    
    * _postprocess_statuses() implementation complete
    
    * _run_pipelines() now makes use of_postprocess_statuses()
    
    * better doc string
    
    * minor fix
    
    * better test coverage
    
    * better tests
    
    * remove superfluous return value
    
    * better name for new procedure
    
    * Update inventory_loader.py
    
    fix spacing
    
    * Update inventory_loader_test.py
    
    tweak test name casing

--- tests/inventory/inventory_loader_test.py
+++ tests/inventory/inventory_loader_test.py
@@ -14,30 +14,98 @@
 
 """Inventory loader script test."""
 
-from datetime import datetime
-
+import copy
 import mock
-import MySQLdb
 import unittest
 
 from tests.unittest_utils import ForsetiTestCase
-from google.cloud.security.common.data_access import dao
-from google.cloud.security.common.data_access import errors
-from google.cloud.security.common.gcp_type import iam_policy
-from google.cloud.security.common.gcp_type import organization
-from google.cloud.security.common.gcp_type import project
-from google.cloud.security.common.gcp_type import resource
 from google.cloud.security.inventory import inventory_loader
 
 
+class _PIPELINE(object):
+    """Mock `Pipeline` class."""
+    def __init__(self, RESOURCE_NAME, status):
+        self.RESOURCE_NAME = RESOURCE_NAME
+        self.status = status
+
+
 class InventoryLoaderTest(ForsetiTestCase):
+    """Unit tests for the `inventory_loader` module."""
 
-    @mock.patch.object(inventory_loader, 'FLAGS')
-    @mock.patch.object(inventory_loader, 'LOGGER')
-    def setUp(self, mock_logger, mock_flags):
-        self.fake_timestamp = '123456'
-        self.mock_logger = mock_logger
+    a_copy = None
+    b_copy = None
 
+    def _take_snapshot(self, a, b):
+        """Take a shallow copy of the data passed for a later comparison."""
+        self.a_copy = copy.copy(a)
+        self.b_copy = copy.copy(b)
+
+    def _equals_the_originals(self, a, b):
+        """True if the data passed equals the most recent snapshots."""
+        return (self.a_copy == a) and (self.b_copy == b)
+
+    def test_adjust_group_members_status_with_failed_groups_inventory(self):
+        pipelines = [
+                _PIPELINE('groups', 'FAILURE'),
+                _PIPELINE('service_accounts', 'SUCCESS'),
+                _PIPELINE('group_members', 'SUCCESS')]
+        statuses = [False, True, True]
+        expected = [False, True, False]
+        self._take_snapshot(pipelines, statuses)
+        inventory_loader._adjust_group_members_status(pipelines, statuses)
+        self.assertEqual(expected, statuses)
+        self.assertEqual('FAILURE', pipelines[2].status)
+        self.assertFalse(self._equals_the_originals(pipelines, statuses))
+
+    def test_adjust_group_members_status_with_groups_inventory_success(self):
+        pipelines = [
+                _PIPELINE('groups', 'SUCCESS'),
+                _PIPELINE('service_accounts', 'SUCCESS'),
+                _PIPELINE('group_members', 'SUCCESS')]
+        statuses = [True] * 3
+        self._take_snapshot(pipelines, statuses)
+        inventory_loader._adjust_group_members_status(pipelines, statuses)
+        self.assertTrue(self._equals_the_originals(pipelines, statuses))
+
+    def test_adjust_group_members_status_without_groups_inventory(self):
+        pipelines = [
+                _PIPELINE('instances', 'SUCCESS'),
+                _PIPELINE('service_accounts', 'SUCCESS'),
+                _PIPELINE('group_members', 'FAILURE')]
+        statuses = [True, True, False]
+        self._take_snapshot(pipelines, statuses)
+        inventory_loader._adjust_group_members_status(pipelines, statuses)
+        self.assertTrue(self._equals_the_originals(pipelines, statuses))
+
+    def test_adjust_group_members_status_with_both_groups_and_members_failed(self):
+        pipelines = [
+                _PIPELINE('groups', 'FAILURE'),
+                _PIPELINE('service_accounts', 'FAILURE'),
+                _PIPELINE('group_members', 'FAILURE')]
+        statuses = [False] * 3
+        self._take_snapshot(pipelines, statuses)
+        inventory_loader._adjust_group_members_status(pipelines, statuses)
+        self.assertTrue(self._equals_the_originals(pipelines, statuses))
+
+    def test_adjust_group_members_status_without_group_members_inventory(self):
+        pipelines = [
+                _PIPELINE('instances', 'SUCCESS'),
+                _PIPELINE('service_accounts', 'SUCCESS'),
+                _PIPELINE('groups', 'FAILURE')]
+        statuses = [True, True, False]
+        self._take_snapshot(pipelines, statuses)
+        inventory_loader._adjust_group_members_status(pipelines, statuses)
+        self.assertTrue(self._equals_the_originals(pipelines, statuses))
+
+    def test_adjust_group_members_status_with_failed_group_members_inventory(self):
+        pipelines = [
+                _PIPELINE('groups', 'SUCCESS'),
+                _PIPELINE('service_accounts', 'SUCCESS'),
+                _PIPELINE('group_members', 'FAILURE')]
+        statuses = [True, True, False]
+        self._take_snapshot(pipelines, statuses)
+        inventory_loader._adjust_group_members_status(pipelines, statuses)
+        self.assertTrue(self._equals_the_originals(pipelines, statuses))
 
 if __name__ == '__main__':
     unittest.main()
