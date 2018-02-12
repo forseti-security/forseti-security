d04e5d0730e2efd860e96b15bc12119a54cfe30d
Joe Cheuk <joecheuk@google.com>
2018-02-12 13:24:51 -0800

    log the datetime formatter when there is an error parsing the datetime

--- google/cloud/forseti/common/util/parser.py
+++ google/cloud/forseti/common/util/parser.py
@@ -64,8 +64,9 @@ def format_timestamp(timestamp_str, datetime_formatter):
             .parse(timestamp_str)
             .strftime(datetime_formatter))
     except (TypeError, ValueError) as e:
-        LOGGER.warn('Unable to parse/format timestamp: %s\n%s',
-                    timestamp_str, e)
+        LOGGER.warn('Unable to parse/format timestamp: %s\n,'
+                    ' datetime_formatter: %s\n%s',
+                    timestamp_str, datetime_formatter, e)
         formatted_timestamp = None
     return formatted_timestamp
 
