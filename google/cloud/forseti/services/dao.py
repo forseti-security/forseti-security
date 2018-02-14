adfffbf96ce931267a1c217bb0ddf9cdb0ffa0a3
Joe Cheuk <joecheuk@google.com>
2018-02-14 10:39:57 -0800

    Increase column size for table

--- google/cloud/forseti/services/dao.py
+++ google/cloud/forseti/services/dao.py
@@ -236,7 +236,7 @@ def define_model(model_name, dbengine, model_seed):
         policy_update_counter = Column(Integer, default=0)
         display_name = Column(String(256), default='')
         email = Column(String(256), default='')
-        data = Column(Text)
+        data = Column(Text(16777215))
 
         parent_type_name = Column(
             String(128),
