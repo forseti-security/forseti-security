02050e6fded6d7c7fa79f557839a7f57e821da45
Muharem Hrnjadovic <altair@google.com>
2018-02-14 10:09:46 +0100

    add failing test

--- tests/scanner/audit/data/test_rules.py
+++ tests/scanner/audit/data/test_rules.py
@@ -411,3 +411,24 @@ RULES10 = {
         },
     ]
 }
+
+# Requiring projects to have owners from a specific domain, for context see
+# also https://github.com/GoogleCloudPlatform/forseti-security/issues/799
+RULES11 = {
+    'rules': [
+        {
+            'name': 'All projects must have an owner from my domain',
+            'mode': 'required',
+            'resource': [{
+                    'type': 'project',
+                    'applies_to': 'self',
+                    'resource_ids': ['*']
+                }],
+            'inherit_from_parents': True,
+            'bindings': [{
+                    'role': 'roles/owner',
+                    'members': ['domain:@xyz.edu']
+                }]
+        },
+    ]
+}
