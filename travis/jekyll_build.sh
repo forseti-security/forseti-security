d87afbe562988117e4c3fab15447b011fd90ed6a
mirons-google <mirons-google@users.noreply.github.com>
2018-02-27 16:06:23 -0600

    Fix script.

--- travis/jekyll_build.sh
+++ travis/jekyll_build.sh
@@ -4,8 +4,9 @@ if  [ -z ${JGT+x} ]; then
     # We don't have access to the encrypted vars.
     bundle exec jekyll build
 else:
-  # We do have access to the encrypted vars.
-  JEKYLL_GITHUB_TOKEN=$JGTbundle exec jekyll build
+    # We do have access to the encrypted vars.
+    JEKYLL_GITHUB_TOKEN=$JGTbundle exec jekyll build
+fi
 
 bundle exec htmlproofer --check-img-http --check-opengraph --check-html \
 --check-favicon --report-missing-names --report-script-embeds \
