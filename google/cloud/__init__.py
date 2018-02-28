8da0ce3ec4ef7f7fc49c36c0ba0d9091a67e0ee6
mirons-google <mirons-google@users.noreply.github.com>
2018-02-28 09:37:49 -0600

    Cleanup.

--- google/cloud/__init__.py
+++ google/cloud/__init__.py
@@ -13,3 +13,8 @@
 # limitations under the License.
 
 """Google Cloud."""
+
+try:
+  __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+  __path__ = __import__('pkgutil').extend_path(__path__, __name__)
