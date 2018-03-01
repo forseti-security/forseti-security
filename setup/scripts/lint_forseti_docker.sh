d83d6366c0abbba26067c19c94a3b001558a0ee4
mirons-google <mirons-google@users.noreply.github.com>
2018-03-01 10:07:16 -0600

    checkpoint

--- setup/scripts/lint_forseti_docker.sh
+++ setup/scripts/lint_forseti_docker.sh
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "Running pylint."
+echo -n "Running pylint."
 
 docker -l error exec -it build /bin/bash -c "pylint --rcfile=pylintrc google/ setup/"
