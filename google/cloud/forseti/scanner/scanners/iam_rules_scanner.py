07aab1c68990b8e3d9c85a7387247fa1e0828cce
Muharem Hrnjadovic <altair@google.com>
2018-03-01 15:59:29 +0100

    Turn on _add_bucket_ancestor_bindings() function

--- google/cloud/forseti/scanner/scanners/iam_rules_scanner.py
+++ google/cloud/forseti/scanner/scanners/iam_rules_scanner.py
@@ -288,5 +288,6 @@ class IamPolicyScanner(base_scanner.BaseScanner):
         """Runs the data collection."""
 
         policy_data, resource_counts = self._retrieve()
+        _add_bucket_ancestor_bindings(policy_data)
         all_violations = self._find_violations(policy_data)
         self._output_results(all_violations, resource_counts)
