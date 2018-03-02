15d917f35369b00e41becad4aae5d2798c44e69a
mirons-google <mirons-google@users.noreply.github.com>
2018-03-02 17:01:39 -0600

    cleanup test.

--- tests/services/model/importer/importer_test.py
+++ tests/services/model/importer/importer_test.py
@@ -112,15 +112,6 @@ class ImporterTest(ForsetiTestCase):
              'gsuite_enabled': True
              },
             model_description)
-        #{
-        #    "pristine": true,
-        #    "source": "inventory",
-        #    "source_info": {
-        #        "inventory_index_id": "2018-02-02T01:47:39.16075"
-        #    },
-        #    "source_root": "organization/111222333",
-        #    "gsuite_enabled": True
-        #}
 
 if __name__ == '__main__':
     unittest.main()
