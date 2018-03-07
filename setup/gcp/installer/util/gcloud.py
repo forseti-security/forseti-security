caf5de38ab03ef3e911c6552401f973ec20ec204
Joe Cheuk <joecheuk@google.com>
2018-03-07 10:29:35 -0800

    Added quiet flag to gcloud compute ssh command

--- setup/gcp/installer/util/gcloud.py
+++ setup/gcp/installer/util/gcloud.py
@@ -790,7 +790,7 @@ def check_vm_init_status(vm_name, zone):
 
     _, out, _ = utils.run_command(
         ['gcloud', 'compute', 'ssh', vm_name,
-         '--zone', zone, '--command', check_script_executed])
+         '--zone', zone, '--command', check_script_executed, '--quiet'])
 
     if 'Execution of startup script finished' in out:
         return True
