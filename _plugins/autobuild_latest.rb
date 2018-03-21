3c7c146d2c4fafa83ccd21b6ac4d9090c0651dbf
Denzel Morris <denzel.morris1@gmail.com>
2018-03-21 16:38:18 -0400

    Document the plugins

--- _plugins/autobuild_latest.rb
+++ _plugins/autobuild_latest.rb
@@ -1,9 +1,23 @@
+# This builds the _docs/latest/ pseudo-directory and updates the inverted index
+# for versions to support the version dropdown on docs pages.
+#
+# 1. The docs are moved over from _docs/_latest/ to _docs/latest
+# 2. Tombstone files for docs that existed in a previous version but no longer
+#    exist in latest are generated in _docs/latest
+# 3. The inverted version index is built
+#
+# This all done transparently behind-the-scenes such that _docs/latest is only
+# seen and used by the Jekyll build process.
 require 'jekyll'
 
+# At the start of Jekyll's build process, create a pseudo _docs/latest directory
+# to represent the latest/ documentation URLs
 Jekyll::Hooks.register :site, :after_reset do |site|
   `./scripts/jekyll_site_pre_render_hook.sh`
 end
 
+# After Jekyll has written the static website to disk at _www/www, then remove
+# the pseudo _docs/latest directory.
 Jekyll::Hooks.register :site, :post_write do |site|
   `rm -rf _docs/latest/`
 end
--- _plugins/exclude_from_watch.rb
+++ _plugins/exclude_from_watch.rb
@@ -1,3 +1,20 @@
+# There exists a set of directories that we want to (1) include in the Jekyll
+# build but (2) NOT trigger another build when they are modified. Primarily,
+# this is used when auto-generating files for Jekyll build usage. It prevents
+# an infinite loop where the auto-generation continues to trigger a new Jekyll
+# build.
+#
+# For example: when auto-generating the _docs/latest pseudo-directory, these new
+# files cause Jekyll's watch functionality to believe files have been modified
+# and trigger a new build. When triggering a new build, these files are auto-
+# generated again, which causes Jekyll's watch functionality to believe they've
+# been modified... and so on.
+#
+# We break this cycle by including these files in the build, but excluding them
+# from triggering a new build on watch.
+#
+# We accomplish this by hooking and overriding ("monkeypatching") jekyll-watch
+# gem's functionality.
 require 'jekyll-watch'
 module Jekyll
   module Watcher
