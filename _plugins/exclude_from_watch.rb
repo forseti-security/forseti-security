e5de40768362df339956e7c19d30406b53376765
Denzel Morris <denzel.morris1@gmail.com>
2018-03-21 18:01:47 -0400

    Add apache license to scripts and plugins

--- _plugins/exclude_from_watch.rb
+++ _plugins/exclude_from_watch.rb
@@ -1,3 +1,17 @@
+# Copyright 2017 The Forseti Security Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # There exists a set of directories that we want to (1) include in the Jekyll
 # build but (2) NOT trigger another build when they are modified. Primarily,
 # this is used when auto-generating files for Jekyll build usage. It prevents
--- scripts/create_new_version_from_latest.sh
+++ scripts/create_new_version_from_latest.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Copyright 2017 The Forseti Security Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Create a new version (for a release) of the documentation from _docs/_latest/.
 # Add the new version to (1) Jekyll config, (2) the docs categories data struct-
@@ -153,7 +166,8 @@ function main() {
     echo "Updating site links within '_docs/${RELEASE_VERSION}/' ..."
     update_docs_links
 
-    echo "Snapshotting latest includes to '_includes/docs/${RELEASE_VERSION}' ..."
+    echo "Snapshotting latest includes to '_includes/docs/${RELEASE_VERSION}'
+        ..."
     snapshot_includes
 
     echo "Updating site links within '_includes/docs/${RELEASE_VERSION}' ..."
