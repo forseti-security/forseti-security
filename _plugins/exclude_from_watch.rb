72f9d503372a345513c0aa37e6c7bd29b62fd2ca
Denzel Morris <denzel.morris1@gmail.com>
2018-03-26 13:04:24 -0400

    Separate out into versioned directories (#1127)
    * Separate out into versioned directories
    
    * Support versioning in URL structure and categories
    
    * Scaffolding for version dropdown on docs page
    
    * Create script for tombstone file generation
    
    * Hook latest autobuild into Jekyll
    
    * Ready for versioning
    
    * Initial pass at versioning for v1.1
    
    * Version document categories as well
    
    * Remove spurious find-and-replace for doc links
    
    * Fixup links
    
    * Finalize initial versioning
    
    * Bring scripts/ inline with style guide
    
    * Change btn-raised to btn-secondary
    
    * Document the plugins
    
    * Remove extraneous doc_categories
    
    * Fixup sync_latest_version
    
    * Remove extraneous colon from jekyll_build
    
    * Add apache license to scripts and plugins
    
    * Add comment about autogen to doc_categories
    
    * Update license year from 2017 to 2018
    
    * Use string continuation for bash string
    
    * Add executable bit for travis build script
    
    * Remove two broken hyperlinks
    
    * Add license to travis build script

---
+++ _plugins/exclude_from_watch.rb
@@ -0,0 +1,43 @@
+# Copyright 2018 The Forseti Security Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# There exists a set of directories that we want to (1) include in the Jekyll
+# build but (2) NOT trigger another build when they are modified. Primarily,
+# this is used when auto-generating files for Jekyll build usage. It prevents
+# an infinite loop where the auto-generation continues to trigger a new Jekyll
+# build.
+#
+# For example: when auto-generating the _docs/latest pseudo-directory, these new
+# files cause Jekyll's watch functionality to believe files have been modified
+# and trigger a new build. When triggering a new build, these files are auto-
+# generated again, which causes Jekyll's watch functionality to believe they've
+# been modified... and so on.
+#
+# We break this cycle by including these files in the build, but excluding them
+# from triggering a new build on watch.
+#
+# We accomplish this by hooking and overriding ("monkeypatching") jekyll-watch
+# gem's functionality.
+require 'jekyll-watch'
+module Jekyll
+  module Watcher
+    # replace old method by new method
+    # new method is now :custom_excludes
+    # overridden method is now :old_custom_excludes
+    alias_method :old_custom_excludes, :custom_excludes
+    def custom_excludes(options)
+      Array(options['exclude'] + options['exclude_from_watch']).map { |e| Jekyll.sanitized_path(options['source'], e) }
+    end
+  end
+end
---
+++ scripts/generate_tombstone_files.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Copyright 2018 The Forseti Security Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Generate pages ("tombstones") for files that existed in a previous version of
+# the docs, but no longer exist in latest.
+
+function silent_pushd() {
+    pushd $1 1>/dev/null 2>&1
+}
+
+function silent_popd() {
+    popd 1>/dev/null 2>&1
+}
+
+function all_version_directories_except_latest() {
+    find _docs -mindepth 1 -maxdepth 1 -not -path */_latest -type d
+}
+
+function latest_version_files() {
+    silent_pushd _docs/_latest
+        find . -type f
+    silent_popd
+}
+
+function all_version_files() {
+    for dir in $(all_version_directories_except_latest); do
+        silent_pushd $dir
+            find . -type f
+        silent_popd
+    done 
+}
+
+function set_union_of_all_version_files() {
+    all_version_files | sort | uniq
+}
+
+#######################################
+# Find files that existed in previous
+# versions but no longer exist in the
+# latest version.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   array - filenames
+#######################################
+function tombstone_files() {
+    comm -23 <(set_union_of_all_version_files) <(latest_version_files | sort)
+}
+
+function generate_tombstone_files() {
+    echo "Creating tombstones for:"
+    for file in $(tombstone_files); do
+        local filename="${file#./}"
+        printf "\t${filename}\n"
+
+        local latest_tombstone_file="_docs/latest/${filename}"
+        
+        local directory
+        directory="$(dirname "${latest_tombstone_file}")"
+
+        mkdir -p "${directory}" && \
+        cp -u scripts/data/tombstone_template.md "${latest_tombstone_file}"
+    done
+}
+
+function main() {
+    generate_tombstone_files
+}
+
+main "$@"
