a37220a13d84745f32b08b3bd0f37bd38a5d35f3
Andrew Hoying <ahoying@google.com>
2018-03-27 13:30:44 -0700

    Fix role configuration.

--- setup/gcp/installer/util/constants.py
+++ setup/gcp/installer/util/constants.py
@@ -91,7 +91,7 @@ GCP_READ_IAM_ROLES = [
     'roles/appengine.appViewer',
     'roles/bigquery.dataViewer',
     'roles/servicemanagement.quotaViewer',
-    'serviceusage.serviceUsageConsumer',
+    'roles/serviceusage.serviceUsageConsumer',
     'roles/cloudsql.viewer'
 ]
 
