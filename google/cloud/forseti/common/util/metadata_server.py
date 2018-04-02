b72b6100a72bcf87d3cb0b186b2bdae86547364f
Andrew Hoying <ahoying@google.com>
2018-04-02 09:02:17 -0700

    Fix lint.

--- google/cloud/forseti/common/util/metadata_server.py
+++ google/cloud/forseti/common/util/metadata_server.py
@@ -63,7 +63,7 @@ def _issue_http_request(method, path, headers):
     try:
         http_client.request(method, path, headers=headers)
         return http_client.getresponse()
-    except (socket.error,  httplib.HTTPException) as e:
+    except (socket.error, httplib.HTTPException) as e:
         LOGGER.error('Error with request: %s', e)
         raise errors.MetadataServerHttpError
 
