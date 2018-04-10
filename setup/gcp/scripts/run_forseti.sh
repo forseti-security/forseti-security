e1ba4189475f4cf7f5cc65b088c229b9a5885f8b
Joe Cheuk <joecheuk@google.com>
2018-04-10 13:54:21 -0700

    Removed unused comments

--- setup/gcp/scripts/run_forseti.sh
+++ setup/gcp/scripts/run_forseti.sh
@@ -46,9 +46,6 @@ forseti inventory create --import_as ${MODEL_NAME}
 echo "Finished running Forseti inventory."
 sleep 5s
 
-# TODO: Ultimately we want the inventory create command to block until the model is finished building.
-# link to the issue: https://github.com/GoogleCloudPlatform/forseti-security/issues/1296
-
 # Waiting for models to be finished building.
 GET_MODEL_STATUS="forseti model get ${MODEL_NAME} | python -c \"import sys, json; print json.load(sys.stdin)['status']\""
 MODEL_STATUS=`eval $GET_MODEL_STATUS`
