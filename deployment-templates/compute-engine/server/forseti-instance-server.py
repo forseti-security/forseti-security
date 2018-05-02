289e44efc54051a6e39bc8b0ba290c213f79631d
joecheuk <joecheuk@google.com>
2018-05-02 16:10:58 -0700

    Updated gce host and docker to use ubuntu image version 18.04, added google cloud sdk installation in the startup script (#1485)
    * Updated image version to ubuntu-1604-lts
    
    * Added assume yes when installing google sdk
    
    * Updated ubuntu version in docker image

--- deployment-templates/compute-engine/server/forseti-instance-server.py
+++ deployment-templates/compute-engine/server/forseti-instance-server.py
@@ -119,6 +119,7 @@ exec 2>&1
 # Ubuntu update.
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get --assume-yes install google-cloud-sdk
 
 USER_HOME=/home/ubuntu
 
