b828d056e7cec24f63b69f42d82346796f951305
Joe Cheuk <joecheuk@google.com>
2018-05-04 14:01:32 -0700

    Addressed PR comment

--- deployment-templates/compute-engine/server/forseti-instance-server.py
+++ deployment-templates/compute-engine/server/forseti-instance-server.py
@@ -212,7 +212,7 @@ USER=ubuntu
 # queue up the jobs.
 # If the cron job failed the acquire lock on the process, it will log a warning message to syslog.
 
-(echo "{run_frequency} /usr/bin/flock -n /tmp/forseti_cron_runner.lock $FORSETI_HOME/setup/gcp/scripts/run_forseti.sh || echo \"Warning: previous cron job 'run_forseti.sh' is still running and the new cron job is attempting to run, exiting...\" 2>&1 | logger") | crontab -u $USER -
+(echo "{run_frequency} /usr/bin/flock -n /tmp/forseti_cron_runner.lock $FORSETI_HOME/setup/gcp/scripts/run_forseti.sh || echo \"[forseti-security] Warning: New Forseti cron job will not be started, because previous Forseti job is still running.\" 2>&1 | logger") | crontab -u $USER -
 echo "Added the run_forseti.sh to crontab under user $USER"
 
 echo "Execution of startup script finished"
