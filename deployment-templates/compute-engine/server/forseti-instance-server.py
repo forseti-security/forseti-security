fe0ad3097b9548592d03b344c794e63f1b6bf08d
Henry Chang <henryc@google.com>
2018-05-14 13:15:43 -0700

    revert chmod change

--- deployment-templates/compute-engine/server/forseti-instance-server.py
+++ deployment-templates/compute-engine/server/forseti-instance-server.py
@@ -162,7 +162,6 @@ pip install -q --upgrade -r requirements.txt
 # Setup Forseti logging
 touch /var/log/forseti.log
 chown ubuntu:root /var/log/forseti.log
-
 cp {forseti_home}/configs/logging/fluentd/forseti.conf /etc/google-fluentd/config.d/forseti.conf
 cp {forseti_home}/configs/logging/logrotate/forseti /etc/logrotate.d/forseti
 chmod 644 /etc/logrotate.d/forseti
