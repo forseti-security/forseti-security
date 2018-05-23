f24d6e7b9332dac39947bbeda494a8c5428f0814
Joe Cheuk <joecheuk@google.com>
2018-05-22 18:06:59 -0700

    Pass identifer to client if both client and server are installed at the same time

--- install/gcp/gcp_installer.py
+++ install/gcp/gcp_installer.py
@@ -110,6 +110,9 @@ def run():
         # If the user didn't specify a type, install both server and client
         forseti_server = ForsetiServerInstaller(server_config)
         instructions = forseti_server.run_setup(final_setup=False)
+        # Server and client will have the same identifier to keep them
+        # consistent.
+        client_config.identifier = server_config.identifier
         ForsetiClientInstaller(client_config, forseti_server).run_setup(
             setup_continuation=True,
             previous_instructions=instructions)
