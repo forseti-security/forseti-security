4744e04b368ff4e12e588f335c46cd35078c782a
Joe Cheuk <joecheuk@google.com>
2018-05-24 14:45:48 -0700

    Updated logs

--- google/cloud/forseti/services/inventory/storage.py
+++ google/cloud/forseti/services/inventory/storage.py
@@ -660,9 +660,9 @@ class Storage(BaseStorage):
             self.inventory_index = self._open(existing_id)
         else:
             self.inventory_index = self._create()
+            self.session.commit() # commit only on create.
 
         self.opened = True
-        self.session.commit()
         if not self.readonly:
             self.session.begin_nested()
         return self.inventory_index.id
