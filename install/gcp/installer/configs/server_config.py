9c2ed22109e9a4a74fe78163ddf71acb07d456ea
angelzamir@google.com <angelzamir@google.com>
2018-05-31 16:28:31 -0500

    Remove whitespace

--- install/gcp/installer/configs/server_config.py
+++ install/gcp/installer/configs/server_config.py
@@ -21,7 +21,7 @@ class ServerConfig(Config):
     """Forseti installer server config object."""
 
     # pylint: disable=too-many-instance-attributes
-    
+
     def __init__(self, **kwargs):
         """Initialize.
 
