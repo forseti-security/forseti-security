c94896230a51d51cb2fdfe594b346ccfbb652204
Joe Cheuk <joecheuk@google.com>
2018-06-20 10:15:40 -0700

    removed trailing whitespace

--- google/cloud/security/common/gcp_api/container.py
+++ google/cloud/security/common/gcp_api/container.py
@@ -228,7 +228,7 @@ class ContainerClient(object):
                 string
               ],
             }
-            
+
         Raises:
             ApiExecutionError: ApiExecutionError is raised if the call to the
                 GCP API fails
