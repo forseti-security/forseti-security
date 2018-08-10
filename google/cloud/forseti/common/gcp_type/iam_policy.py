ce21ffeaf70f97b2cdec65e0b012ccda28092618
Henry Chang <henryc@google.com>
2018-08-10 16:21:08 -0700

    fix line too long

--- google/cloud/forseti/common/gcp_type/iam_policy.py
+++ google/cloud/forseti/common/gcp_type/iam_policy.py
@@ -344,7 +344,8 @@ class IamPolicyMember(object):
 
         # Bucket IAM supports a special "allUsers" member, whose value is simply
         # "allUsers", without a colon separator and a second fragment.
-        if (self.type == self.ALL_USERS and other_member.type == self.ALL_USERS):
+        if (self.type == self.ALL_USERS and
+            other_member.type == self.ALL_USERS):
             return True
 
         # Match if:
