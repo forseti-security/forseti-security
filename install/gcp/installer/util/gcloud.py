1c1d9beb939970cc83b77dc48069c861d42591fb
johnrevans6 <johnre@hawaii.edu>
2018-08-13 13:19:17 -0700

    Update gcloud.py

--- install/gcp/installer/util/gcloud.py
+++ install/gcp/installer/util/gcloud.py
@@ -715,7 +715,8 @@ def create_firewall_rule(rule_name,
                            '--target-service-accounts',
                            format_service_accounts, '--priority',
                            str(priority), '--direction', direction.value,
-                           '--rules', format_rules, '--network', vpc_host_network]
+                           '--rules', format_rules, 
+                           '--network', vpc_host_network]
     if source_ranges:
         gcloud_command_args.extend(['--source-ranges', source_ranges])
 
