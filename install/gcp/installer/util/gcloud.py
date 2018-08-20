76aadce86cba216b0c93e0e01e7908099ef2b537
John Evans <johnrevans6@gmail.com>
2018-08-20 14:51:13 -0700

    Readd rules now that branch is clean

--- install/gcp/installer/util/gcloud.py
+++ install/gcp/installer/util/gcloud.py
@@ -53,9 +53,8 @@ def get_gcloud_info():
 
 def set_network_host_project_id(self):
     """Get the host project."""
-    if not self.config.vpc_host_project_id:
-        self.get_project()
-        self.vpc_host_project_id = self.project_id
+    if not self.config.vpc_host_project_id:  
+        self.config.vpc_host_project_id, _, _ = get_gcloud_info()
     print('VPC Host Project %s' % self.config.vpc_host_project_id)
 
 def activate_service_account(key_file):
