87477ebfd5a840e8d55958cb36ca05db6fd12998
joecheuk <joecheuk@google.com>
2018-08-29 10:44:02 -0700

    Removed upgrade option (#1965)

--- install/gcp/installer/forseti_server_installer.py
+++ install/gcp/installer/forseti_server_installer.py
@@ -22,9 +22,7 @@ from forseti_installer import ForsetiInstaller
 from util import constants
 from util import files
 from util import gcloud
-from util import merge_engine
 from util import utils
-from util import upgradeable_resources
 
 
 class ForsetiServerInstaller(ForsetiInstaller):
@@ -37,7 +35,6 @@ class ForsetiServerInstaller(ForsetiInstaller):
     resource_root_id = None
     access_target = None
     target_id = None
-    migrate_from_v1 = False
     user_can_grant_roles = True
 
     def __init__(self, config, previous_installer=None):
@@ -50,7 +47,6 @@ class ForsetiServerInstaller(ForsetiInstaller):
         """
         super(ForsetiServerInstaller, self).__init__(config,
                                                      previous_installer)
-        self.v1_config = None
 
     def preflight_checks(self):
         """Pre-flight checks for server instance."""
@@ -59,44 +55,9 @@ class ForsetiServerInstaller(ForsetiInstaller):
         self.config.generate_cloudsql_instance()
         self.get_email_settings()
         gcloud.enable_apis(self.config.dry_run)
-        forseti_v1_name = None
-        if not self.config.dry_run:
-            _, zone, forseti_v1_name = gcloud.get_vm_instance_info(
-                constants.REGEX_MATCH_FORSETI_V1_INSTANCE_NAME, try_match=True)
-        if forseti_v1_name:
-            utils.print_banner('Found A V1 Installation:'
-                               ' Importing Configuration And Rules.')
-            # v1 instance exists, ask if the user wants to port
-            # the conf/rules settings from v1.
-            self.prompt_v1_configs_migration()
-        if self.migrate_from_v1:
-            self.v1_config = upgradeable_resources.ForsetiV1Configuration(
-                self.project_id, forseti_v1_name, zone)
-            self.v1_config.fetch_information_from_gcs()
-            self.populate_config_info_from_v1()
         self.determine_access_target()
         print('Forseti will be granted write access and required roles to: '
               '{}'.format(self.resource_root_id))
-
-    def prompt_v1_configs_migration(self):
-        """Ask the user if they want to migrate conf/rule files
-        from v1 to v2."""
-        choice = ''
-        while choice != 'y' and choice != 'n':
-            choice = raw_input(
-                constants.QUESTION_SHOULD_MIGRATE_FROM_V1).lower()
-        self.migrate_from_v1 = choice == 'y'
-
-    def populate_config_info_from_v1(self):
-        """Retrieve the v1 configuration object."""
-        v1_conf_global = self.v1_config.config.get('global')
-        self.config.sendgrid_api_key = v1_conf_global.get('sendgrid_api_key')
-        self.config.gsuite_superadmin_email = v1_conf_global.get(
-            'domain_super_admin_email')
-        self.config.notification_recipient_email = v1_conf_global.get(
-            'email_recipient')
-        self.config.notification_sender_email = v1_conf_global.get(
-            'email_sender')
 
     def deploy(self, deployment_tpl_path, conf_file_path, bucket_name):
         """Deploy Forseti using the deployment template.
@@ -119,10 +80,6 @@ class ForsetiServerInstaller(ForsetiInstaller):
             default_rule_values = self.get_rule_default_values()
             files.update_rule_files(default_rule_values,
                                     constants.RULES_DIR_PATH)
-
-            # Replace new rules if necessary.
-            if self.migrate_from_v1:
-                self.replace_with_old_rules()
 
             print('Copying the default Forseti rules to:\n\t{}'.format(
                 os.path.join(bucket_name, 'rules')))
@@ -150,17 +107,6 @@ class ForsetiServerInstaller(ForsetiInstaller):
             self.create_firewall_rules()
 
         return success, deployment_name
-
-    def replace_with_old_rules(self):
-        """Replace new rules with old rules.
-
-        This is very specific for migration from v1 to v2 because we don't
-        want to modify the rule files that user defined in v1.
-        """
-        for v1_rule in self.v1_config.rules:
-            new_rule_path = os.path.join(constants.RULES_DIR_PATH,
-                                         v1_rule.file_name)
-            files.write_data_to_yaml_file(v1_rule.data, new_rule_path)
 
     def create_firewall_rules(self):
         """Create firewall rules for Forseti server instance."""
@@ -199,52 +145,6 @@ class ForsetiServerInstaller(ForsetiInstaller):
             self.config.vpc_host_network,
             '0.0.0.0/0')
 
-    def generate_forseti_conf(self):
-        """Generate Forseti conf file.
-
-        If self.migrate_from_v1 is True, pull the v1 configuration
-        file and merge it with v2 template.
-
-        Returns:
-            str: Forseti configuration file path.
-        """
-
-        forseti_conf_path = super(
-            ForsetiServerInstaller, self).generate_forseti_conf()
-
-        if self.migrate_from_v1:
-            new_conf = files.read_yaml_file_from_local(forseti_conf_path)
-            fields_to_ignore = [
-                'db_host', 'db_user', 'db_name',
-                'inventory', 'output_path', 'gcs_path',
-                'groups_service_account_key_file',
-                'domain_super_admin_email',
-                'max_admin_api_calls_per_100_seconds',
-                'max_appengine_api_calls_per_second',
-                'max_bigquery_api_calls_per_100_seconds',
-                'max_cloudbilling_api_calls_per_60_seconds',
-                'max_compute_api_calls_per_second',
-                'max_container_api_calls_per_100_seconds',
-                'max_crm_api_calls_per_100_seconds',
-                'max_iam_api_calls_per_second',
-                'max_results_admin_api',
-                'max_sqladmin_api_calls_per_100_seconds',
-                'resources']
-            field_identifiers = {'scanners': 'name',
-                                 'resources': 'resource',
-                                 'pipelines': 'name'}
-            merge_engine.merge_object(merge_from=self.v1_config.config,
-                                      merge_to=new_conf,
-                                      fields_to_ignore=fields_to_ignore,
-                                      field_identifiers=field_identifiers)
-
-            # Fields that have changed categories cannot be merged,
-            # swap them here instead.
-            self._swap_config_fields(self.v1_config.config, new_conf)
-
-            files.write_data_to_yaml_file(new_conf, forseti_conf_path)
-
-        return forseti_conf_path
 
     def format_firewall_rule_name(self, rule_name):
         """Format firewall rule name.
@@ -429,57 +329,3 @@ class ForsetiServerInstaller(ForsetiInstaller):
                 if notifier['name'] == 'gcs_violations':
                     return notifier['configuration']['gcs_path']
         return ''
-
-    @staticmethod
-    def _swap_config_fields(old_config, new_config):
-        """Swapping fields. This will work for all v1 migrating to v2.
-
-        Note: new_config will get modified.
-
-        Args:
-            old_config (dict): Old configuration.
-            new_config (dict): New configuration.
-        """
-        # pylint: disable=too-many-locals
-        # Some fields have been renamed from v1 to v2
-        # This is a map to map names from v2 back to v1
-        global_to_inventory = [
-            'domain_super_admin_email'
-        ]
-
-        new_conf_inventory = new_config['inventory']
-
-        old_config_global = ({} if 'global' not in old_config
-                             else old_config['global'])
-
-        for field in global_to_inventory:
-            if field in old_config_global:
-                new_conf_inventory[field] = (old_config_global[field]
-                                             or new_conf_inventory[field])
-
-        old_notifier_resources = old_config['notifier']['resources']
-        new_notifier_resources = new_config['notifier']['resources']
-        new_scanner_gcs_path = ForsetiServerInstaller._get_gcs_path(
-            new_notifier_resources)
-
-        resource_name_to_index = {}
-
-        for idx, old_resource in enumerate(old_notifier_resources):
-            resource_name_to_index[old_resource['resource']] = idx
-
-        for idx, resource in enumerate(new_notifier_resources):
-            resource_name = resource['resource']
-            if resource_name in resource_name_to_index:
-                # if resource_name is in the old notifier section, replace
-                # the new notifier section with the old one and update the
-                # values accordingly.
-                new_notifier_resources[idx] = old_notifier_resources[
-                    resource_name_to_index[resource_name]]
-                resource_to_update = new_notifier_resources[idx]
-                resource_to_update['notifiers'] = resource_to_update.pop(
-                    'pipelines')
-                for notifier in resource_to_update['notifiers']:
-                    notifier['name'] = notifier['name'].replace('_pipeline', '')
-                    if notifier['name'] == 'gcs_violations':
-                        notifier['configuration']['gcs_path'] = (
-                            new_scanner_gcs_path)
