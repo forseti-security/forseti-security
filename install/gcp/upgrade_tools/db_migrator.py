03c4c1988769cdb6078df20ea4386ad3d875b0d7
Joe Cheuk <joecheuk@google.com>
2018-09-18 10:36:34 -0700

    pylint updates

--- install/gcp/upgrade_tools/db_migrator.py
+++ install/gcp/upgrade_tools/db_migrator.py
@@ -1,3 +1,21 @@
+# Copyright 2017 The Forseti Security Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Forseti db migrator."""
+
+from __future__ import print_function
+
 import sys
 
 # Importing migrate.changeset adds some new methods to existing SQLAlchemy
@@ -16,7 +34,7 @@ DEFAULT_DB_CONN_STR = 'mysql://root@127.0.0.1:3306/forseti_security'
 LOGGER = logger.get_logger(__name__)
 
 
-class ColumnAction:
+class ColumnAction(object):
     """Column action class."""
     DROP = 'DROP'
     CREATE = 'CREATE'
@@ -44,7 +62,7 @@ def drop_column(table, column):
     column.drop(table)
 
 
-column_action_mapping = {ColumnAction.DROP: drop_column,
+COLUMN_ACTION_MAPPING = {ColumnAction.DROP: drop_column,
                          ColumnAction.CREATE: create_column}
 
 
@@ -73,36 +91,48 @@ def migrate_schema(engine, base):
         get_schema_update_actions = getattr(subclass,
                                             schema_update_actions_method,
                                             None)
-        if (callable(get_schema_update_actions) and
-                subclass.__tablename__ in tables):
-            LOGGER.info('Updating table %s', subclass.__tablename__)
-            # schema_update will require the Table object.
-            table = tables.get(subclass.__tablename__)
-            schema_update_mapping = get_schema_update_actions()
-            for column_action, columns in schema_update_mapping.iteritems():
-                column_action = column_action.upper()
-                if column_action in column_action_mapping:
-                    for column in columns:
-                        try:
-                            column_action_mapping.get(column_action)(table,
-                                                                     column)
-                        except OperationalError:
-                            LOGGER.info('Failed to update db schema, table=%s',
-                                        subclass.__tablename__)
-                        except Exception:
-                            LOGGER.exception(
-                                'Unexpected error happened when attempting '
-                                'to update database schema, table: %s',
-                                subclass.__tablename__)
-                else:
-                    LOGGER.warn('Columns: %s, ColumnAction %s doesn\'t '
-                                'exist.', columns, column_action)
+        if (not callable(get_schema_update_actions) or
+                subclass.__tablename__ not in tables):
+            LOGGER.warn('Method: %s is not callable or Table: %s doesn\t '
+                        'exist', schema_update_actions_method,
+                        subclass.__tablename__)
+            continue
+        LOGGER.info('Updating table %s', subclass.__tablename__)
+        # schema_update will require the Table object.
+        table = tables.get(subclass.__tablename__)
+        schema_update_mapping = get_schema_update_actions()
+        for column_action, columns in schema_update_mapping.iteritems():
+            column_action = column_action.upper()
+            if column_action in COLUMN_ACTION_MAPPING:
+                for column in columns:
+                    try:
+                        COLUMN_ACTION_MAPPING.get(column_action)(table,
+                                                                 column)
+                    except OperationalError:
+                        LOGGER.info('Failed to update db schema, table=%s',
+                                    subclass.__tablename__)
+                    except Exception:  # pylint: disable=broad-except
+                        LOGGER.exception(
+                            'Unexpected error happened when attempting '
+                            'to update database schema, table: %s',
+                            subclass.__tablename__)
+            else:
+                LOGGER.warn('Columns: %s, ColumnAction %s doesn\'t '
+                            'exist.', columns, column_action)
 
 
 def _find_subclasses(cls):
+    """Find all the subclasses of a class.
+
+    Args:
+        cls (class): The parent class.
+
+    Returns:
+        list: Subclasses of the given parent class.
+    """
     results = []
-    for sc in cls.__subclasses__():
-        results.append(sc)
+    for subclass in cls.__subclasses__():
+        results.append(subclass)
     return results
 
 
@@ -112,10 +142,10 @@ if __name__ == '__main__':
     print (sys.argv)
     DB_CONN_STR = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_DB_CONN_STR
 
-    sql_engine = general_dao.create_engine(DB_CONN_STR,
+    SQL_ENGINE = general_dao.create_engine(DB_CONN_STR,
                                            pool_recycle=3600)
 
-    declaritive_bases = [scanner_dao.BASE, inventory_dao.BASE]
+    DECLARITIVE_BASES = [scanner_dao.BASE, inventory_dao.BASE]
 
-    for base in declaritive_bases:
-        migrate_schema(sql_engine, base)
+    for declaritive_base in DECLARITIVE_BASES:
+        migrate_schema(SQL_ENGINE, declaritive_base)
