6bf35080d89a4b73ff16868f23ffd6eb5fea1293
Rehana Devani <rdevani@google.com>
2018-09-24 12:54:55 -0700

    parenthesis fixed

--- google/cloud/forseti/common/util/logger.py
+++ google/cloud/forseti/common/util/logger.py
@@ -26,7 +26,7 @@ from google.cloud.forseti import __version__
 DEFAULT_LOG_FMT = ('%(asctime)s %(levelname)s '
                    '%(name)s(%(funcName)s): %(message).1024s')
 
-SYSLOG_LOG_FMT = ('%(levelname)s [forseti-security] %(__version__)s '
+SYSLOG_LOG_FMT = ('%(levelname)s [forseti-security] %[__version__]s '
                   '%(name)s(%(funcName)s): %(message).1024s')
 
 # %(asctime)s is used as the marker by multiline parser to determine
