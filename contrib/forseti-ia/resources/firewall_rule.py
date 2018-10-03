11c813097b2ade475138806118dc64441dc651fb
joecheuk <joecheuk@google.com>
2018-10-03 15:12:25 -0700

    (contrib/forseti-ia) Firewall rule data processing and added sample dataset (#2057)
    * updates
    
    * Added logic to generate flattened firewall rule objects from API response json string.
    
    * updates
    
    * Converting fw rules to dataframe
    
    * Addressed PR comments.

--- contrib/forseti-ia/resources/firewall_rule.py
+++ contrib/forseti-ia/resources/firewall_rule.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 
 """
 Flatten the following attributes in firewall rules.
@@ -25,56 +26,210 @@ This will generate 4 Firewall rule objects
 - action: allowed, IPProtocol: udp, ports: 50051
 - action: allowed, IPProtocol: udp, ports: 50051
 
+Source or destination
+You specify either a source or a destination, but not both, depending 
+on the direction of the firewall you create:
+
+For ingress (inbound) rules, the target parameter specifies the destination 
+VMs for traffic; you cannot use the destination parameter. You specify 
+the source by using the source parameter.
+
+For egress (outbound) rules, the target parameter specifies the source VMs 
+for traffic; you cannot use the source parameter. You specify the destination 
+by using the destination parameter.
+
+You cannot mix and match service accounts and network tags in any firewall rule:
+
+You cannot use target service accounts and target tags together in any firewall 
+rule (ingress or egress).
+
+{
+"allowed": [{"IPProtocol": "tcp", "ports": ["50051"]}],
+"creationTimestamp": "2018-02-28T21:49:07.739-08:00",
+"description": "",
+"direction": "INGRESS", 
+"disabled": false, 
+"id": "1002375335368075964", 
+"kind": "compute#firewall", 
+"name": "forseti-server-allow-grpc-20180228211432", 
+"network": "https://www.googleapis.com/compute/beta/projects/joe-project-p2/global/networks/default", 
+"priority": 0, 
+"selfLink": "https://www.googleapis.com/compute/beta/projects/joe-project-p2/global/firewalls/forseti-server-allow-grpc-20180228211432", 
+"sourceRanges": ["10.128.0.0/9"], 
+"targetServiceAccounts": ["forseti-gcp-server-1432@joe-project-p2.iam.gserviceaccount.com"]
+}
 
 """
 
 
-DIRECTION_ENCODING = {
-    'INGRESS': [0, 1],
-    'EGRESS': [1, 0]
-}
-
-
 class FirewallRule(object):
     """Flattened firewall rule."""
-    def __init__(self):
-        self.creation_timestamp = ''
-        # "creationTimestamp": "2018-02-28T21:49:07.739-08:00"
+    def __init__(self,
+                 creation_timestamp,
+                 priority,
+                 ip_addr,
+                 ip_bits,
+                 identifier,
+                 action,
+                 ip_protocol,
+                 ports,
+                 direction,
+                 disabled):
+        self.creation_timestamp = creation_timestamp
+        self.priority = priority
+        self.ip_addr = ip_addr
+        self.ip_bits = ip_bits
+        self.identifier = identifier
+        self.action = action
+        self.ip_protocol = ip_protocol
+        self.ports = ports
+        self.direction = direction
+        self.disabled = disabled
 
-        self.description = ''
-        # "description": ""
+    def to_dict(self):
+        """Convert to dictionary object.
 
-        self.name = ''
-        # "name": "forseti-server-allow-grpc-20180228211432",
+        Returns:
+            dict: Dictionary representation of the object.
+        """
+        return {
+            'creation_timestamp': self.creation_timestamp,
+            'priority': self.priority,
+            'ip_addr': self.ip_addr,
+            'ip_bits': self.ip_bits,
+            'identifier': self.identifier,
+            'action': self.action,
+            'ip_protocol': self.ip_protocol,
+            'ports': self.ports,
+            'direction': self.direction,
+            'disabled': self.disabled,
+        }
 
-        self.network = ''
-        # "network": "https://www.googleapis.com/compute/beta/projects/joe-project-p2/global/networks/default"
+    @classmethod
+    def from_json(cls, gcp_firewall_rule):
+        """Generate a list of flattened firewall rule objects based
+         on the given firewall resource data in string format.
 
-        self.priority = 0
-        # "priority": 0
+         Args:
+            gcp_firewall_rule (str): Firewall rule resource data,
+                in JSON string format.
 
-        self.source_ranges = 0
-        # "sourceRanges": ["10.128.0.0/9"]
+        Returns:
+             list: A list of flattened firewall rule objects.
+         """
+        json_dict = json.loads(gcp_firewall_rule)
 
-        self.destination_ranges = 0
-        # "destinationRanges": ["10.128.0.0/9"]
+        action = 'allowed'
 
-        self.source_tags = []
+        if action not in json_dict:
+            action = 'denied'
 
-        self.target_tags = []
+        identifiers = []
 
-        self.source_service_accounts = []
+        if 'targetServiceAccounts' in json_dict:
+            identifiers = json_dict.get('targetServiceAccounts', [])
+        elif 'targetTags' in json_dict:
+            identifiers = json_dict.get('targetTags', [])
 
-        self.target_service_accounts = []
-        # targetServiceAccounts": ["forseti-gcp-server-1432@joe-project-p2.iam.gserviceaccount.com"]}"
+        direction = json_dict.get('direction')  # INGREE OR EGRESS
 
-        self.allowed = []
-        # "allowed": [{"IPProtocol": "tcp", "ports": ["50051"]}]
+        if direction.upper() == 'INGRESS':
+            ip_addrs_cidr = json_dict.get('sourceRanges')
+        else:
+            ip_addrs_cidr = json_dict.get('destinationRanges')
 
-        self.denied = []
+        ip_addr_bits_list = [ip_addr_cidr.split('/') for ip_addr_cidr in ip_addrs_cidr]
 
-        self.direction = ''
-        # "direction": "INGRESS"
+        protocol_mappings = json_dict.get(action, [])
 
-        self.disabled = True
-        # "disabled": false
+        flattened_firewall_rules = []
+
+        creation_timestamp = json_dict.get('creationTimestamp')
+        priority = json_dict.get('priority')
+        disabled = False if json_dict.get('disabled') == 'false' else True
+
+        for identifier in identifiers:
+            for (ip_addr, ip_bits) in ip_addr_bits_list:
+                for protocol_mapping in protocol_mappings:
+                    ip_protocols = protocol_mapping.get('IPProtocol', [])
+                    corresponding_ports = protocol_mapping.get('ports', [])
+
+                    flattened_ports = cls._flatten_ports(corresponding_ports)
+                    for ip_protocol in ip_protocols:
+                        flattened_firewall_rules.append(
+                            FirewallRule(
+                                creation_timestamp,
+                                priority,
+                                ip_addr,
+                                ip_bits,
+                                identifier,
+                                action,
+                                ip_protocol,
+                                flattened_ports,
+                                direction,
+                                disabled
+                            ))
+        return flattened_firewall_rules
+
+    @classmethod
+    def flatten_firewall_rules(cls, gcp_firewall_rules):
+        """Flatten all the gcp firewall rule data.
+
+        Args:
+            gcp_firewall_rules (list): A list of firewall rules.
+
+        Returns:
+            list: A list of flattened firewall rules.
+        """
+        results = []
+        for gcp_firewall_rule in gcp_firewall_rules:
+            try:
+                results.extend(cls.from_json(gcp_firewall_rule))
+            except Exception as e:
+                print e
+                print gcp_firewall_rule
+        return results
+
+    @classmethod
+    def _flatten_ports(cls, ports):
+        """Flatten the list of ports.
+
+        Example:
+            Input: ["1-5", "50051"]
+            Output: ["1", "2", "3", "4", "5", "50051"]
+
+        Args:
+            corresponding_ports (list): List of corresponding ports.
+
+        Returns:
+            list: Flattened ports.
+        """
+
+        flattened_ports = []
+
+        # Type of representation that can be in the ports
+        # Range - e.g. "0-5"
+        # Single port - e.g. "50051"
+        # Empty - e.g. [] - this represents all ports.
+
+        for port in ports:
+            if '-' in port:
+                # This is port range, flatten the range.
+                port_range = port.split('-')
+                start = int(port_range[0])
+                end = int(port_range[1])
+                flattened = [i for i in range(start, end+1)]
+                flattened_ports += flattened
+            else:
+                # Single port.
+                flattened_ports.append(int(port))
+
+        # Cast the list to set to remove duplicates,
+        # and cast it back to list.
+        flattened_ports = list(set(flattened_ports))
+
+        # Max port is 65535.
+        # if not flattened_ports:
+        #    flattened_ports = [i for i in range(0, 65536)]
+
+        return flattened_ports
