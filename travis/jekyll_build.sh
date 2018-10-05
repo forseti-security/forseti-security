6c2489c3ca7b66b50c460cc317c0639c77a1646b
Joe Cheuk <joecheuk@google.com>
2018-10-04 17:02:20 -0700

    travis updates

--- travis/jekyll_build.sh
+++ travis/jekyll_build.sh
@@ -30,7 +30,6 @@ fi
 bundle exec htmlproofer --check-img-http --check-html \
 --check-favicon --report-missing-names --report-script-embeds \
 --url-ignore '/GoogleCloudPlatform/forseti-security/edit/' \
---file-ignore '/develop/reference/' \
-./_www/www
+--file-ignore '/develop/reference/' './_www/www/'
 
 exit ${return_code}
