718906b085fe668442c7ae4669d39aea1b62cc40
red2k18 <rdevani@google.com>
2018-10-09 22:53:15 +0000

    adding span to crawler.py

--- google/cloud/forseti/services/inventory/crawler.py
+++ google/cloud/forseti/services/inventory/crawler.py
@@ -19,11 +19,14 @@ import threading
 import time
 from Queue import Empty, Queue
 
+from opencensus.trace import attributes_helper
+from opencensus.trace import execution_context
+from opencensus.trace import span as span_module
+
 from google.cloud.forseti.common.util import logger
 from google.cloud.forseti.services.inventory.base import crawler
 from google.cloud.forseti.services.inventory.base import gcp
 from google.cloud.forseti.services.inventory.base import resources
-
 
 LOGGER = logger.get_logger(__name__)
 
@@ -299,7 +302,10 @@ def run_crawler(storage,
     Returns:
         QueueProgresser: The progresser implemented in inventory
     """
-
+    _tracer = execution_context.get_opencensus_tracer()
+    _span = tracer.start.span()
+    _span.name = '[requests]{}'.format(requests_func._name_)
+    _span.span_kind = span_module.spanKind.CLIENT
     client_config = config.get_api_quota_configs()
     client_config['domain_super_admin_email'] = config.get_gsuite_admin_email()
 
@@ -315,4 +321,6 @@ def run_crawler(storage,
     progresser = crawler_impl.run(resource)
     # flush the buffer at the end to make sure nothing is cached.
     storage.commit()
+    _tracer.add_attribute_to_current_span(HTTP_STATUS_CODE, str(result.status_code))
+    _tracer.end_span()
     return progresser
