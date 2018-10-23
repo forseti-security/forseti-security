b7b42c56094a259314e5de07ea57e930242283ab
Olivier Cervello <ocervello@google.com>
2018-10-23 14:49:05 -0500

    Update

--- google/cloud/forseti/common/opencensus/tracing.py
+++ google/cloud/forseti/common/opencensus/tracing.py
@@ -50,11 +50,11 @@ def create_client_interceptor(endpoint):
     """
     exporter = create_exporter()
     tracer = Tracer(exporter=exporter)
-    LOGGER.debug("Span context (client interceptor): %s", tracer.span_context)
+    LOGGER.info("Span context (client interceptor): %s", tracer.span_context)
     interceptor = client_interceptor.OpenCensusClientInterceptor(
         tracer,
         host_port=endpoint)
-    LOGGER.debug("Span context (client interceptor - after init): %s", tracer.span_context)
+    LOGGER.info("Span context (client interceptor - after init): %s", tracer.span_context)
     return interceptor
 
 def create_server_interceptor(extras=True):
@@ -74,7 +74,7 @@ def create_server_interceptor(extras=True):
     interceptor = server_interceptor.OpenCensusServerInterceptor(
         sampler,
         exporter)
-    LOGGER.debug("Span context (client interceptor - after init): %s", execution_context.get_opencensus_tracer().span_context)
+    LOGGER.info("Span context (client interceptor - after init): %s", execution_context.get_opencensus_tracer().span_context)
     return interceptor
 
 def trace_integrations(integrations=None):
@@ -94,7 +94,7 @@ def trace_integrations(integrations=None):
         integrations,
         tracer)
     LOGGER.info('Tracing integration libraries: %s', integrated_libraries)
-    LOGGER.debug("Span context (integrations): %s", tracer.span_context)
+    LOGGER.info("Span context (integrations): %s", tracer.span_context)
     return integrated_libraries
 
 
