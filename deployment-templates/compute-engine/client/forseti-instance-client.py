2ccf8b4f0e5c260b64ad047d1ae62a81e9f907b6
Henry Chang <henryc@google.com>
2018-11-01 12:28:20 -0700

    Revert "Add OpenCensus instrumentation for tracing" (#2171)
    * Revert "Fix applies_to in location scanner rule (#2161)"
    
    This reverts commit ca65e4122212caa84d287545fef61f5dd1b44218.
    
    * Revert "Add OpenCensus instrumentation for tracing (#1926)"
    
    This reverts commit 3ec22634866686964a418856fd6f947573cc63ae.

--- deployment-templates/compute-engine/client/forseti-instance-client.py
+++ deployment-templates/compute-engine/client/forseti-instance-client.py
@@ -133,9 +133,6 @@ pip install --upgrade pip==9.0.3
 pip install -q --upgrade setuptools wheel
 pip install -q --upgrade -r requirements.txt
 
-# Install instrumentation libs
-pip install .[tracing]
-
 # Install Forseti
 python setup.py install
 
