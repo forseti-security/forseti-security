ea7a5c4bad6b22430b3c77807f10b926fd35929f
Angel Sun <angelsun@google.com>
2018-11-07 10:50:41 -0800

    delete unnecessary setup in unit test

--- tests/scanner/resource_util_test.py
+++ tests/scanner/resource_util_test.py
@@ -18,22 +18,6 @@ from tests.unittest_utils import ForsetiTestCase
 class ResourceUtilTest(ForsetiTestCase):
     """Test misc."""
 
-    def setUp(self):
-        """Set up."""
-        fake_global_configs = {
-            'crm': {'max_calls': 4, 'period': 1.2}}
-
-        mock.patch.object(google.auth, 'default', return_value=(
-            mock.Mock(spec_set=credentials.Credentials), 'test-project'))
-
-        mock_creds = api_helpers.get_delegated_credential('user1@eaxmple.com', mock.MagicMock())
-
-        self.crm_api_client = crm.CloudResourceManagerClient(
-            global_configs=fake_global_configs,
-            use_rate_limiter=False,
-            credentials=mock_creds)
-        self.project_id = fake_crm_responses.FAKE_PROJECT_ID
-
     def test_cast_to_gcp_resources(self):
         """Validate get_project_ancestry_resources() with test project."""
         expected_cast_resources = "[project<id=forseti-system-test,parent=None>, folder<id=3333333,parent=None>, organization<id=2222222,parent=None>]"
