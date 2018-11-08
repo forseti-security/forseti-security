c3e1148e128f301dc0e0f81eca59650f2516ee6a
red2k18 <41486977+red2k18@users.noreply.github.com>
2018-11-08 11:12:20 -0800

    Update crawler.py

--- google/cloud/forseti/services/inventory/crawler.py
+++ google/cloud/forseti/services/inventory/crawler.py
@@ -313,7 +313,7 @@ def run_crawler(storage,
         QueueProgresser: The progresser implemented in inventory
     """
     tracer = config.service_config.tracer
-    tracing.start_span(tracer, "inventory", "run_crawler")
+    span = tracing.start_span(tracer, "inventory", "run_crawler")
     client_config = config.get_api_quota_configs()
     client_config['domain_super_admin_email'] = config.get_gsuite_admin_email()
 
@@ -329,5 +329,5 @@ def run_crawler(storage,
     progresser = crawler_impl.run(resource)
     # flush the buffer at the end to make sure nothing is cached.
     storage.commit()
-    tracing.end_span(tracer, **progresser.__dict__)
+    tracing.end_span(tracer, span, **progresser.__dict__)
     return progresser
