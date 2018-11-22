839c69503078505f8ebcabcbebf573535a4e598f
Olivier Cervello <ocervello@google.com>
2018-11-21 19:31:33 -0600

    Add attributes to crawler spans

--- google/cloud/forseti/services/inventory/crawler.py
+++ google/cloud/forseti/services/inventory/crawler.py
@@ -135,8 +135,8 @@ class Crawler(crawler.Crawler):
             raise
         else:
             progresser.on_new_object(resource)
-        #finally:
-            #tracing.end_span(self.config.tracer, **attrs)
+        finally:
+            tracing.end_span(self.config.tracer, **attrs)
 
     def dispatch(self, callback):
         """Dispatch crawling of a subtree.
