47cefbb866973b02e8bad731e84efa0211b9003b
Michael Irons <mirons@google.com>
2018-11-30 12:14:07 -0800

    ignore the boostrap cdn url.

--- travis/jekyll_build.sh
+++ travis/jekyll_build.sh
@@ -30,6 +30,7 @@ fi
 bundle exec htmlproofer --check-img-http --check-html \
 --check-favicon --report-missing-names --report-script-embeds \
 --url-ignore '/GoogleCloudPlatform/forseti-security/edit/' \
+--url-ignore 'https://maxcdn.bootstrapcdn.com/bootstrap/' \
 --file-ignore '/develop/reference/' './_www/www/'
 
 exit ${return_code}
