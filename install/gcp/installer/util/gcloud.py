0cdb302f0cce1a67d68d09ae5192e37e39a3a638
hshin-g <shinhannah@google.com>
2019-01-17 10:15:43 -0800

    change gcloud command to gcloud iam roles list --project

--- install/gcp/installer/util/gcloud.py
+++ install/gcp/installer/util/gcloud.py
@@ -291,8 +291,7 @@ def grant_server_svc_acct_project_roles(target_id,
         user_can_grant_roles, roles)
 
     return_code, out, err = utils.run_command([
-        'gcloud', 'iam', 'service-accounts', 'get-iam-policy',
-        gcp_service_account])
+        'gcloud', 'iam', 'roles', 'list', '--project', project_id])
     if return_code:
         print(return_code)
         print(err)
