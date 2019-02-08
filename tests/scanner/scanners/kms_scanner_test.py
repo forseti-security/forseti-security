7be28ab23039996b45446f59a56edb127e1be84c
Rehana Devani <rdevani@google.com>
2019-02-07 17:49:37 -0800

    test cases

--- tests/scanner/scanners/kms_scanner_test.py
+++ tests/scanner/scanners/kms_scanner_test.py
@@ -16,10 +16,12 @@
 
 import unittest
 import mock
+import json
 from datetime import datetime
 
 from tests import unittest_utils
 from tests.services.util.db import create_test_engine
+from tests.scanner.test_data import fake_kms_scanner_data
 from google.cloud.forseti.common.gcp_type import resource as resource_mod
 from google.cloud.forseti.scanner.scanners import kms_scanner
 from google.cloud.forseti.common.util.string_formats import TIMESTAMP_MICROS
@@ -37,14 +39,18 @@ The crypto key with creation time over 100 days ago should be flagged as a
 violation but not the other one.
 """
 
+KEY_RING_ID = '4063867491605246570'
+CRYPTO_KEY_ID = '12873861500163377322'
+
 TIME_NOW = datetime.utcnow()
 
-'''
+
 class FakeServiceConfig(object):
 
     def __init__(self):
         engine = create_test_engine()
         self.model_manager = ModelManager(engine)
+
 
 class KMSScannerTest(unittest_utils.ForsetiTestCase):
 
@@ -55,52 +61,44 @@ class KMSScannerTest(unittest_utils.ForsetiTestCase):
             name='kms-scanner-test')
 
         scoped_session, data_access = (
-            cls.service_config.model_manager.get(cls.model_name)
+            cls.service_config.model_manager.get(cls.model_name))
 
         # Add organization to model.
         with scoped_session as session:
-            organization = data.access.add.resource_by_name(
-        session, 'organization/12345', '', True)
+            organization = data_access.add_resource_by_name(
+                session, 'organization/12345', '', True)
 
+            project = data_access.add_resource(session, 'project/foo',
+                                               organization)
 
-        session.commit()
+            key_ring = data_access.add_resource(
+                session, 'kms_keyring/%s' % KEY_RING_ID, project)
 
+            key_ring.data = fake_kms_scanner_data.KEY_RING_DATA
+
+            crypto_key = data_access.add_resource(
+                session, 'kms_cryptokey/%s' % CRYPTO_KEY_ID, key_ring)
+
+            crypto_key.data = fake_kms_scanner_data.NON_ROTATED_CRYPTO_KEY_DATA
+
+            session.commit()
 
     def setUp(self):
-        self.scanner = kms_scanner.KKMSScanner(
+        self.scanner = kms_scanner.KMSScanner(
             {}, {}, self.service_config, self.model_name,
             '', unittest_utils.get_datafile_path(
                 __file__, 'kms_scanner_test_rules.yaml'))
 
     @mock.patch.object(
-        kms_scanner.KKMSScanner,
+        kms_scanner.KMSScanner,
         '_output_results_to_db', autospec=True)
     def test_run_scanner(self, mock_output_results):
-        self.scanner.run()
-        expected_violations = [EXPECTED_VIOLATION, ]
+        violations = self.scanner.run()
+        print('violations:', violations)
+        expected_violations = [json.loads(fake_kms_scanner_data.MATCHED_VIOLATION)]
         mock_output_results.assert_called_once_with(mock.ANY,
                                                     expected_violations)
 
-    def test_retrieve(self):
-        mock_data_access = mock.MagicMock()
-        mock_data_access.scanner_iter.side_effect = _mock_gcp_resource_iter
-        mock_service_config = mock.MagicMock()
-        mock_service_config.model_manager = mock.MagicMock()
-        mock_service_config.model_manager.get.return_value = (
-            mock.MagicMock(), mock_data_access)
-        self.scanner.service_config = mock_service_config
-
-        mock_rule_book = mock.MagicMock()
-        mock_rule_book.get_applicable_resource_types.return_value = set([
-            'organization'])
-        self.scanner.rules_engine.rule_book = mock_rule_book
-
-
-        got = set(self.scanner._retrieve())
-        want = {data.BUCKET, data.PROJECT1}
-        self.assertEqual(got, want)
-
-'''
 
 if __name__ == '__main__':
     unittest.main()
--- tests/scanner/test_data/fake_kms_scanner_data.py
+++ tests/scanner/test_data/fake_kms_scanner_data.py
@@ -15,7 +15,7 @@
 
 from google.cloud.forseti.scanner.audit import kms_rules_engine
 
-ROTATED_CRYPTO_KEY = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
+ROTATED_CRYPTO_KEY_DATA = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
                  '"name":"projects/red2k18-224817/locations/global/keyRings/'
                  'red_key_ring/cryptoKeys/red_key1",'
                  '"nextRotationTime":"2019-07-21T07:00:00Z",'
@@ -23,7 +23,7 @@ ROTATED_CRYPTO_KEY = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
                  '"algorithm":"GOOGLE_SYMMETRIC_ENCRYPTION",'
                  '"createTime":"2019-01-22T23:30:18.939244464Z",'
                  '"generateTie":"2019-01-22T23:30:18.939244464Z",'
-                 '"name":"projects/red2k18-224817/locations/global/keyRings/'
+                 '"name":"projects/red2k18-22487/locations/global/keyRings/'
                  'red_key_ring/cryptoKeys/red_key1/cryptoKeyVersions/1",'
                  '"protectionLevel":"SOFTWARE",'
                  '"state":"ENABLED"},'
@@ -33,7 +33,7 @@ ROTATED_CRYPTO_KEY = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
                  '"algorithm":"GOOGLE_SYMMETRIC_ENCRYPTION",'
                  '"protectionLevel":"SOFTWARE"}}')
 
-NON_ROTATED_CRYPTO_KEY = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
+NON_ROTATED_CRYPTO_KEY_DATA = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
                  '"name":"projects/red2k18-224817/locations/global/keyRings/'
                  'red_key_ring/cryptoKeys/red_key1",'
                  '"nextRotationTime":"2018-07-21T07:00:00Z",'
@@ -50,6 +50,9 @@ NON_ROTATED_CRYPTO_KEY = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
                  '"versionTemplate":{'
                  '"algorithm":"GOOGLE_SYMMETRIC_ENCRYPTION",'
                  '"protectionLevel":"SOFTWARE"}}')
+
+KEY_RING_DATA = ('{"createTime":"2019-01-22T23:29:46.507107968Z",'
+            '"name":"projects/red2k18-224817/locations/global/keyRings/red_key_ring",}')
 
 RESOURCE_DATA = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
                  '"name":"projects/red2k18-224817/locations/global/keyRings/'
@@ -78,7 +81,7 @@ PRIMARY_VERSION = ('"primary":{ '
                  '"protectionLevel":"SOFTWARE",'
                  '"state":"ENABLED"}')
 
-FLATTENED_KMS_SCANNER_VIOLATION = {'resource_id': '12387945269031473611',
+EXPECTED_VIOLATION = {'resource_id': '12387945269031473611',
                       'resource_type': 'kms_cryptokey',
                       'resource_name': 'organization/660570133860/project/red2k18-224817/kms_keyring/4063867491605246570/kms_cryptokey/12387945269031473611/',
                       'full_name': 'kms_cryptokey',
@@ -88,7 +91,7 @@ FLATTENED_KMS_SCANNER_VIOLATION = {'resource_id': '12387945269031473611',
                       'violation_data': 'Key %s was not rotated since %s.',
                       'resource_data': RESOURCE_DATA
                       }
-
+'''
 RuleViolation = kms_rules_engine.Rule.RuleViolation
 
 CRYPTO_KEY_VIOLATIONS = [
@@ -100,7 +103,7 @@ CRYPTO_KEY_VIOLATIONS = [
                   rule_name='All cryptographic keys should be rotated in 100 days',
                   violation_type='CRYPTO_KEY_VIOLATION',
                   primary_version=PRIMARY_VERSION,
-                  next_rotation_time=,
+                  next_rotation_time='2019-07-21T07:00:00Z',
                   rotation_period=100,
                   violation_reason='CRYPTO_KEY_VIOLATION',
                   key_creation_time='2019-01-22T23:30:18.939244464Z',
@@ -112,4 +115,69 @@ GCP_RESOURCES = {
 
 }
 
+'''
 
+RESOURCE_DATA = ('{"createTime":"2019-01-22T23:30:18.939244464Z",'
+                 '"name":"projects/red2k18-224817/locations/global/keyRings/'
+                 'red_key_ring/cryptoKeys/red_key1",'
+                 '"nextRotationTime":"2019-07-21T07:00:00Z",'
+                 '"primary":{ '
+                 '"algorithm":"GOOGLE_SYMMETRIC_ENCRYPTION",'
+                 '"createTime":"2019-01-22T23:30:18.939244464Z",'
+                 '"generateTie":"2019-01-22T23:30:18.939244464Z",'
+                 '"name":"projects/red2k18-224817/locations/global/keyRings/'
+                 'red_key_ring/cryptoKeys/red_key1/cryptoKeyVersions/1",'
+                 '"protectionLevel":"SOFTWARE",'
+                 '"state":"ENABLED"},'
+                 '"purpose":"ENCRYPT_DECRYPT",'
+                 '"rotationPeriod":"15552000s",'
+                 '"versionTemplate":{'
+                 '"algorithm":"GOOGLE_SYMMETRIC_ENCRYPTION",'
+                 '"protectionLevel":"SOFTWARE"}}')
+
+
+
+MATCHED_VIOLATION = ('[{"rule_name": "All cryptographic keys should be rotated in 100 days",'
+                      '"resource_name": "organization/12345/project/foo/kms_keyring/4063867491605246570/kms_cryptokey/12873861500163377322/",' 
+                      '"resource_data": {"createTime": "2019-01-22T23:30:18.939244464Z",' 
+                      '"name": "projects/red2k18-224817/locations/global/keyRings/red_key_ring/cryptoKeys/red_key1",' 
+                      '"nextRotationTime": "2018-07-21T07:00:00Z",'
+                      '"primary": {"algorithm": "GOOGLE_SYMMETRIC_ENCRYPTION",'
+                      '"createTime": "2018-01-22T23:30:18.939244464Z",'
+                      '"generateTie": "2019-01-22T23:30:18.939244464Z",'
+                      '"name": "projects/red2k18-224817/locations/global/keyRings/red_key_ring/cryptoKeys/red_key1/cryptoKeyVersions/1",'
+                      '"protectionLevel": "SOFTWARE",'
+                      '"state": "ENABLED"},'
+                      '"purpose": "ENCRYPT_DECRYPT",'
+                      '"rotationPeriod": "15552000s",'
+                      '"versionTemplate": {"algorithm": "GOOGLE_SYMMETRIC_ENCRYPTION","protectionLevel": "SOFTWARE"}}",'
+                      '"full_name": "kms_cryptokey",'
+                      '"resource_id": "12873861500163377322",'
+                      '"rule_index": 0,'
+                      '"violation_type": "CRYPTO_KEY_VIOLATION",'
+                      '"violation_data": "Key organization/12345/project/foo/kms_keyring/4063867491605246570/kms_cryptokey/12873861500163377322/ was not rotated since 2018-01-22T23:30:18.939244464Z.",'
+                      '"resource_type": "kms_cryptokey"}]'
+                  )
+
+
+MATCHED_VIOLATION = ('{"rule_name": "All cryptographic keys should be rotated in 100 days",'
+                      '"resource_name": "organization/12345/project/foo/kms_keyring/4063867491605246570/kms_cryptokey/12873861500163377322/",' 
+                      '"resource_data": {"createTime": "2019-01-22T23:30:18.939244464Z",' 
+                      '"name": "projects/red2k18-224817/locations/global/keyRings/red_key_ring/cryptoKeys/red_key1",' 
+                      '"nextRotationTime": "2018-07-21T07:00:00Z",'
+                      '"primary": {"algorithm": "GOOGLE_SYMMETRIC_ENCRYPTION",'
+                      '"createTime": "2018-01-22T23:30:18.939244464Z",'
+                      '"generateTie": "2019-01-22T23:30:18.939244464Z",'
+                      '"name": "projects/red2k18-224817/locations/global/keyRings/red_key_ring/cryptoKeys/red_key1/cryptoKeyVersions/1",'
+                      '"protectionLevel": "SOFTWARE",'
+                      '"state": "ENABLED"},'
+                      '"purpose": "ENCRYPT_DECRYPT",'
+                      '"rotationPeriod": "15552000s",'
+                      '"versionTemplate": {"algorithm": "GOOGLE_SYMMETRIC_ENCRYPTION","protectionLevel": "SOFTWARE"}},'
+                      '"full_name": "kms_cryptokey",'
+                      '"resource_id": "12873861500163377322",'
+                      '"rule_index": 0,'
+                      '"violation_type": "CRYPTO_KEY_VIOLATION",'
+                      '"violation_data": "Key organization/12345/project/foo/kms_keyring/4063867491605246570/kms_cryptokey/12873861500163377322/ was not rotated since 2018-01-22T23:30:18.939244464Z.",'
+                      '"resource_type": "kms_cryptokey"}'
+                     )
