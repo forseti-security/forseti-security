e93d38d6fe9bdd58fddce046dd25cbc43f25fd78
red2k18 <41486977+red2k18@users.noreply.github.com>
2019-03-20 10:53:43 -0700

    Deprecating CSCC Alpha support and removing Beta checks (#2647)
    * deprecating alpha support and removing beta check
    
    * removed alpha check
    
    * use_versioned_discovery_doc field is always True
    
    * Removed Alpha API client unit test

--- tests/common/gcp_api/securitycenter_test.py
+++ tests/common/gcp_api/securitycenter_test.py
@@ -38,20 +38,9 @@ class SecurityCenterTest(unittest_utils.ForsetiTestCase):
         """Set up."""
         fake_global_configs = {
             'securitycenter': {'max_calls': 1, 'period': 1.1}}
-        # securitycenter_alpha_api_client is alpha api client
-        cls.securitycenter_alpha_api_client = securitycenter.SecurityCenterClient()
         cls.securitycenter_beta_api_client = securitycenter.SecurityCenterClient(version='v1beta1')
         cls.project_id = 111111
         cls.source_id = 'organizations/111/sources/222'
-
-    @mock.patch.object(
-        google.auth, 'default',
-        return_value=(mock.Mock(spec_set=credentials.Credentials),
-                      'test-project'))
-    def test_no_quota(self, mock_google_credential):
-        """Verify no rate limiter is used if the configuration is missing."""
-        securitycenter_alpha_api_client = securitycenter.SecurityCenterClient()
-        self.assertEqual(None, securitycenter_alpha_api_client.repository._rate_limiter)
 
     def test_create_findings(self):
         """Test create cscc findings."""
@@ -59,13 +48,8 @@ class SecurityCenterTest(unittest_utils.ForsetiTestCase):
         http_mocks.mock_http_response(
             json.dumps(fake_cscc.EXPECTED_CREATE_FINDING_RESULT))
 
-        result = self.securitycenter_alpha_api_client.create_finding(
-            fake_cscc.FAKE_ALPHA_FINDING, fake_cscc.ORGANIZATION_ID)
-        self.assertEquals(fake_cscc.EXPECTED_CREATE_FINDING_RESULT, result)
-
         result = self.securitycenter_beta_api_client.create_finding(
             'fake finding',
-            fake_cscc.ORGANIZATION_ID,
             source_id=self.source_id
             )
         self.assertEquals(fake_cscc.EXPECTED_CREATE_FINDING_RESULT, result)
@@ -74,18 +58,11 @@ class SecurityCenterTest(unittest_utils.ForsetiTestCase):
         """Test create cscc finding raises exception."""
         http_mocks.mock_http_response(fake_cscc.PERMISSION_DENIED, '403')
 
-        # alpha api
-        with self.assertRaises(api_errors.ApiExecutionError):
-            self.securitycenter_alpha_api_client.create_finding(
-                json.loads(fake_cscc.FAKE_ALPHA_FINDING),
-                           fake_cscc.ORGANIZATION_ID)
-
         # beta api
         fake_beta_finding = {'source_properties': {'violation_data': 'foo'}}
         with self.assertRaises(api_errors.ApiExecutionError):
             self.securitycenter_beta_api_client.create_finding(
                 fake_beta_finding,
-                fake_cscc.ORGANIZATION_ID,
                 source_id=self.source_id)
 
 
