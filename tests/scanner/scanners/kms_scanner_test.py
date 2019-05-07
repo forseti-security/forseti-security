7ffef66490fd8ecaf516713c8fa84d7b494e2f6f
hshin-g <shinhannah@google.com>
2019-05-07 14:37:26 -0700

    + Change kms_scanner_test.py Line 228: 1--> 6

--- tests/scanner/scanners/kms_scanner_test.py
+++ tests/scanner/scanners/kms_scanner_test.py
@@ -225,7 +225,7 @@ class KMSScannerTest(unittest_utils.ForsetiTestCase):
         self.scanner.run()
         crypto_key = self.scanner._retrieve()
         violations = self.scanner._find_violations(crypto_key)
-        self.assertEqual(1, len(violations))
+        self.assertEqual(6, len(violations))
         self.assertEqual(1, mock_output_results.call_count)
 
 
