93559add827985ea298e7c536d1dc2c5e851c5f7
Rehana Devani <rdevani@google.com>
2019-06-18 19:12:52 -0700

    fixed pylint error

--- google/cloud/forseti/services/model/importer/importer.py
+++ google/cloud/forseti/services/model/importer/importer.py
@@ -611,7 +611,7 @@ class InventoryImporter(object):
             'kubernetes_cluster': self._convert_kubernetes_cluster,
             'kubernetes_clusterrole': self._convert_kubernetes_clusterrole,
             'kubernetes_clusterrolebinding':
-                self._convert_kubernetes_clusterrole_binding,
+                self._convert_kubernetes_crole_binding,
             'kubernetes_namespace': self._convert_kubernetes_namespace,
             'kubernetes_node': self._convert_kubernetes_node,
             'kubernetes_pod': self._convert_kubernetes_pod,
@@ -808,8 +808,7 @@ class InventoryImporter(object):
                                cached=False,
                                display_key='kubernetesClusterRole')
 
-    def _convert_kubernetes_clusterrole_binding(self,
-                                                 kubernetes_clusterrolebinding):
+    def _convert_kubernetes_crole_binding(self, kubernetes_clusterrolebinding):
         """Convert a Kubernetes ClusterRoleBinding resource to a database
            object.
 
