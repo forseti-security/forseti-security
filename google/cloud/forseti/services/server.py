efa6adf43ce80b6a7caa69d193835d8f258bface
Olivier Cervello <ocervello@google.com>
2019-07-26 11:59:11 +0200

    Add True / False to acceptable truthy / falsy values

--- google/cloud/forseti/services/server.py
+++ google/cloud/forseti/services/server.py
@@ -177,9 +177,9 @@ def str2bool(v):
     """
     if isinstance(v, bool):
        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ('yes', 'true', 'True', 't', 'y', '1'):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ('no', 'false', 'False', 'f', 'n', '0'):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
