013dd049137013db50a87448c6ed5109730385b1
Andrew Hoying <ahoying@google.com>
2019-08-05 16:01:56 -0400

    Stream CloudAsset data straight to sqlite database.
    * Change cloudasset implementation to stream data from GCS to the local
    sqlite database using OS pipes instead of downloading the full file to a
    temporary location and then reading it from there.
    
    This change reduces storage requirements on Forseti Server and allows
    data to be processed only once instead of writing and rereading each row
    of data from the CloudAsset export.

--- tests/services/inventory/crawling_test.py
+++ tests/services/inventory/crawling_test.py
@@ -17,14 +17,15 @@ import copy
 import os
 import unittest
 import unittest.mock as mock
-from sqlalchemy.orm import sessionmaker
 from tests.services.inventory import gcp_api_mocks
-from tests.services.util.db import create_test_engine_with_file
 from tests.services.util.mock import MockServerConfig
 from tests import unittest_utils
-from google.cloud.forseti.common.util import file_loader
+import google.auth
+from google.oauth2 import credentials
+from google.cloud.forseti.common.gcp_api import storage
 from google.cloud.forseti.common.util import logger
 from google.cloud.forseti.services.base.config import InventoryConfig
+from google.cloud.forseti.services.inventory import cai_temporary_storage
 from google.cloud.forseti.services.inventory.storage import initialize
 from google.cloud.forseti.services.inventory.base.progress import Progresser
 from google.cloud.forseti.services.inventory.base.storage import Memory as MemoryStorage
@@ -153,20 +154,19 @@ class CrawlerBase(unittest_utils.ForsetiTestCase):
 
         return result_counts
 
-    def _run_crawler(self, config, has_org_access=True, session=None):
+    def _run_crawler(self, config, has_org_access=True):
         """Runs the crawler with a specific InventoryConfig.
 
         Args:
             config (InventoryConfig): The configuration to test.
             has_org_access (bool): True if crawler has access to the org
                 resource.
-            session (object): An existing sql session, required for testing
-                Cloud Asset API integration.
+            client (object): An API Client implementation, used for CAI testing.
 
         Returns:
             dict: the resource counts returned by the crawler.
         """
-        with MemoryStorage(session=session) as storage:
+        with MemoryStorage() as storage:
             progresser = NullProgresser()
             with gcp_api_mocks.mock_gcp(has_org_access=has_org_access):
                 run_crawler(storage,
@@ -490,10 +490,6 @@ class CloudAssetCrawlerTest(CrawlerBase):
     def setUp(self):
         """Setup method."""
         CrawlerBase.setUp(self)
-        self.engine, self.dbfile = create_test_engine_with_file()
-        session_maker = sessionmaker()
-        self.session = session_maker(bind=self.engine)
-        initialize(self.engine)
         self.inventory_config = InventoryConfig(gcp_api_mocks.ORGANIZATION_ID,
                                                 '',
                                                 {},
@@ -501,42 +497,54 @@ class CloudAssetCrawlerTest(CrawlerBase):
                                                 {'enabled': True,
                                                  'gcs_path': 'gs://test-bucket'}
                                                )
-        self.inventory_config.set_service_config(FakeServerConfig(self.engine))
+        self.inventory_config.set_service_config(
+            FakeServerConfig('mock_engine'))
 
-        # Ensure test data doesn't get deleted
-        self.mock_unlink = mock.patch.object(
-            os, 'unlink', autospec=True).start()
-        self.mock_copy_file_from_gcs = mock.patch.object(
-            file_loader,
-            'copy_file_from_gcs',
-            autospec=True).start()
         self.maxDiff = None
 
-        # Mock copy_file_from_gcs to return correct test data file
-        def _copy_file_from_gcs(file_path, *args, **kwargs):
-            """Fake copy_file_from_gcs."""
-            del args, kwargs
-            if 'resource' in file_path:
-                return os.path.join(TEST_RESOURCE_DIR_PATH,
-                                    'mock_cai_resources.dump')
-            elif 'iam_policy' in file_path:
-                return os.path.join(TEST_RESOURCE_DIR_PATH,
-                                    'mock_cai_iam_policies.dump')
+    def _run_crawler(self, config):
+        """Runs the crawler with a specific InventoryConfig.
 
-        self.mock_copy_file_from_gcs.side_effect = _copy_file_from_gcs
+        Args:
+            config (InventoryConfig): The configuration to test.
+
+        Returns:
+            dict: the resource counts returned by the crawler.
+        """
+        # Mock download to return correct test data file
+        def _fake_download(full_bucket_path, output_file):
+            if 'resource' in full_bucket_path:
+                fake_file = os.path.join(TEST_RESOURCE_DIR_PATH,
+                                         'mock_cai_resources.dump')
+            elif 'iam_policy' in full_bucket_path:
+                fake_file = os.path.join(TEST_RESOURCE_DIR_PATH,
+                                         'mock_cai_iam_policies.dump')
+            with open(fake_file, 'rb') as f:
+                output_file.write(f.read())
+
+        with MemoryStorage() as storage:
+            progresser = NullProgresser()
+            with gcp_api_mocks.mock_gcp() as gcp_mocks:
+                gcp_mocks.mock_storage.download.side_effect = _fake_download
+                run_crawler(storage,
+                            progresser,
+                            config,
+                            parallel=True)
+
+            self.assertEqual(0,
+                             progresser.errors,
+                             'No errors should have occurred')
+
+            return self._get_resource_counts_from_storage(storage)
 
     def tearDown(self):
         """tearDown."""
         CrawlerBase.tearDown(self)
         mock.patch.stopall()
 
-        # Stop mocks before unlinking the database file.
-        os.unlink(self.dbfile)
-
     def test_cai_crawl_to_memory(self):
         """Crawl mock environment, test that there are items in storage."""
-        result_counts = self._run_crawler(self.inventory_config,
-                                          session=self.session)
+        result_counts = self._run_crawler(self.inventory_config)
 
         expected_counts = copy.deepcopy(GCP_API_RESOURCES)
         expected_counts.update({
@@ -600,8 +608,7 @@ class CloudAssetCrawlerTest(CrawlerBase):
             'sqladmin': {'disable_polling': True},
             'storage': {'disable_polling': True},
         }
-        result_counts = self._run_crawler(self.inventory_config,
-                                          session=self.session)
+        result_counts = self._run_crawler(self.inventory_config)
         # Any resource not included in Cloud Asset export will not be in the
         # inventory.
         expected_counts = {
@@ -681,7 +688,7 @@ class CloudAssetCrawlerTest(CrawlerBase):
                                             'gcs_path': 'gs://test-bucket',
                                             'asset_types': asset_types}
                                           )
-        inventory_config.set_service_config(FakeServerConfig(self.engine))
+        inventory_config.set_service_config(FakeServerConfig('fake_engine'))
 
         # Create subsets of the mock resource dumps that only contain the
         # filtered asset types
@@ -707,17 +714,20 @@ class CloudAssetCrawlerTest(CrawlerBase):
 
         with unittest_utils.create_temp_file(filtered_assets) as resources:
             with unittest_utils.create_temp_file(filtered_iam) as iam_policies:
-                def _copy_file_from_gcs(file_path, *args, **kwargs):
-                    """Fake copy_file_from_gcs."""
-                    del args, kwargs
-                    if 'resource' in file_path:
-                        return resources
-                    elif 'iam_policy' in file_path:
-                        return iam_policies
-                self.mock_copy_file_from_gcs.side_effect = _copy_file_from_gcs
-                with MemoryStorage(session=self.session) as storage:
+                # Mock download to return correct test data file
+                def _fake_download(full_bucket_path, output_file):
+                    if 'resource' in full_bucket_path:
+                        fake_file = resources
+                    elif 'iam_policy' in full_bucket_path:
+                        fake_file = iam_policies
+                    with open(fake_file, 'rb') as f:
+                        output_file.write(f.read())
+
+                with MemoryStorage() as storage:
                     progresser = NullProgresser()
                     with gcp_api_mocks.mock_gcp() as gcp_mocks:
+                        gcp_mocks.mock_storage.download.side_effect = (
+                            _fake_download)
                         run_crawler(storage,
                                     progresser,
                                     inventory_config)
