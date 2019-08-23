ecb2107cb4a4db95d43d6fb0a321c432bdb6c588
Henry Chang <henryc@google.com>
2019-08-23 09:43:06 -0700

    Fix Flaky Firewall Test (#3118)
    * Fix flaky test
    
    * fix replay test
    
    * add comment
    
    * add print
    
    * add print
    
    * add print
    
    * add print
    
    * remove print
    
    (cherry picked from commit cde7b832dade29d61d2f334cf100966c256ca56b)

--- tests/common/gcp_api/compute_test.py
+++ tests/common/gcp_api/compute_test.py
@@ -154,6 +154,9 @@ class ComputeTest(unittest_utils.ForsetiTestCase):
         """Test create/patch/update/delete firewall rule."""
         with mock.patch.object(self.gce_api_client.repository.firewalls,
                                'read_only', return_value=True):
+            # Hardcode _baseUrl to prevent flakiness from external call.
+            self.gce_api_client.repository.firewalls.gcp_service._baseUrl = (
+                'https://www.googleapis.com/compute/v1/projects/')
             method = getattr(self.gce_api_client,
                              '{}_firewall_rule'.format(verb))
             results = method(self.project_id,
