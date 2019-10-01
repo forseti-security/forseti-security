34340253414afbab858e41b4baa9d2b9874adfcb
joecheuk <joecheuk@google.com>
2019-10-01 14:15:52 -0700

    Updated iter() method in storage.py to use slice() instead of yield_p… (#3255)
    * Updated iter() method in storage.py to use slice() instead of yield_per().
    
    * updates.
    
    * Lowercased generator in docstring.

--- google/cloud/forseti/services/inventory/storage.py
+++ google/cloud/forseti/services/inventory/storage.py
@@ -43,6 +43,7 @@ from google.cloud.forseti.common.util import date_time
 from google.cloud.forseti.common.util import logger
 from google.cloud.forseti.common.util.index_state import IndexState
 # pylint: disable=line-too-long
+from google.cloud.forseti.services import dao
 from google.cloud.forseti.services import utils
 from google.cloud.forseti.services.inventory.base.storage import Storage as BaseStorage
 from google.cloud.forseti.services.scanner.dao import ScannerIndex
@@ -717,8 +718,9 @@ class DataAccess(object):
             fetch_category (Categories): The category of data to fetch.
             with_parent (bool): Join parent with results, yield tuples.
 
-        Yields:
-            object: Single row object or child/parent if 'with_parent' is set.
+        Returns:
+            generator: Generator of single row object or child/parent
+                if 'with_parent' is set.
         """
         filters = [Inventory.inventory_index_id == inventory_index_id,
                    Inventory.category == fetch_category]
@@ -740,8 +742,7 @@ class DataAccess(object):
 
         base_query = base_query.order_by(Inventory.id.asc())
 
-        for row in base_query.yield_per(PER_YIELD):
-            yield row
+        return dao.page_query(base_query)
 
     @classmethod
     def get_root(cls, session, inventory_index_id):
