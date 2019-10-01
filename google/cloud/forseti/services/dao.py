dfc3234b1a9f85fa0231f8b687651a58fd3e7f11
joecheuk <joecheuk@google.com>
2019-10-01 10:30:36 -0700

    Updated scanner_iter method to use query.slice() instead of yield_per and increased the cv audit size to 100mb per. (#3244)
    * Use query.slice() instead of yield_per to avoid losing connection to mysql server.
    
    * updates
    
    * Updated to to audit every 100mb.
    
    * Updated block size to 4096.

--- google/cloud/forseti/services/dao.py
+++ google/cloud/forseti/services/dao.py
@@ -62,7 +62,7 @@ from google.cloud.forseti.common.util import logger
 LOGGER = logger.get_logger(__name__)
 
 POOL_RECYCLE_SECONDS = 300
-PER_YIELD = 1024
+PER_YIELD = 4096
 
 
 def generate_model_handle():
@@ -729,18 +729,28 @@ def define_model(model_name, dbengine, model_seed):
             Yields:
                 Resource: resource that match the query
             """
-
-            qry = (
+            query = (
                 session.query(Resource)
                 .filter(Resource.type == resource_type)
                 .options(joinedload(Resource.parent))
                 .enable_eagerloads(True))
 
             if parent_type_name:
-                qry = qry.filter(Resource.parent_type_name == parent_type_name)
+                query = query.filter(
+                    Resource.parent_type_name == parent_type_name)
 
-            for resource in qry.yield_per(PER_YIELD):
-                yield resource
+            block_size = PER_YIELD
+            block_number = 0
+
+            resources = query.slice(block_number * block_size,
+                                    (block_number + 1) * block_size).all()
+
+            while resources:
+                for resource in resources:
+                    yield resource
+                block_number += 1
+                resources = query.slice(block_number * block_size,
+                                        (block_number + 1) * block_size).all()
 
         @classmethod
         def scanner_fetch_groups_settings(cls, session, only_iam_groups):
