d066aa1076e3b1ad6e26506b2ffae9ec4817ea47
red2k18 <rdevani@google.com>
2019-10-04 19:17:48 -0400

    [PoC] Continuous Integration and Release Automation (#3202)
    * Added tests
    
    * Added tests
    
    * Added terraform configuration and encrypted keys
    
    * Updated travis.yml
    
    * Updated path
    
    * Optimized tests
    
    * updated travis and script
    
    * renamed integration to integration_tests
    
    * addressed comments
    
    * Added tests to check database for inventory
    
    * Reverted script changes
    
    * updating wait time so that server is ready
    
    * updated wait time to 900 seconds
    
    * modified script
    
    * waiting for 900 seconds
    
    * integration tests will only run for cron job
    
    * Optimmized tests by creating inventory and model only once for all tests
    
    * updated script and renamed fixture
    
    * Optimized tests further and organized in separate files based on modules
    
    * Added new line
    
    * Updated SQL queries
    
    * nits
    
    * Running the unit tests before the integration tests and added another assertion to the test for model delete
    
    * Integration tests will be executed nightly
    
    * Added database check for projects
    
    * Installing mysql-client in the setup phase and not in the tests
    
    * Run integration tests nightly
    
    * Updated SQL query to scan only ACTIVE projects
    
    * Adding back the condition to run integration tests nightly
    
    * Added KMS resources and asserting against them
    
    * Running travis integration tests on this PR
    
    * Renamed KMS resources
    
    * Randomized names
    
    * Fixed string interpolation error and added stdout checks
    
    * No to duplication of terraform configuration files
    
    * Added assertion for model use command
    
    * No to prevent destroying resources
    
    * No to checks on stderr
    
    * Dropped stderr checks from model
    
    * Added comments
    
    * nit
    
    * Removed duplicated command

---
+++ integration_tests/fixtures/forseti/scripts/wait-for-forseti.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Block until the Forseti startup script has finished running.
+
+MAX_WAIT=900
+
+echo "Waiting for up to $MAX_WAIT seconds for Forseti to be ready."
+
+for _ in $(seq 1 $MAX_WAIT); do
+  if [[ -f /etc/profile.d/forseti_environment.sh ]]; then
+    echo "Waiting for startup script to complete"
+    sleep 60
+    exit 0
+  else
+    sleep 10
+  fi
+done
+
+echo "Forseti was not ready after $MAX_WAIT seconds!"
+exit 1
+
