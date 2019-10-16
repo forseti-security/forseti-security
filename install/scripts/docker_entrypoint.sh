6135d8d65127b2db8a5cbd51cd56b176fae6571c
joecheuk <joecheuk@google.com>
2019-10-16 09:15:33 -0700

    Updated docker_entrypoint.sh with sql user and password.

--- install/scripts/docker_entrypoint.sh
+++ install/scripts/docker_entrypoint.sh
@@ -107,7 +107,7 @@ start_server(){
 
     forseti_server \
     --endpoint ${SERVER_HOST}:${SERVER_PORT} \
-    --forseti_db "mysql+pymysql://root@${SQL_HOST}:${SQL_PORT}/forseti_security" \
+    --forseti_db "mysql+pymysql://${SQL_DB_USER}:${SQL_DB_PASSWORD}@${SQL_HOST}:${SQL_PORT}/forseti_security" \
     --services ${SERVICES} \
     --config_file_path "/forseti-security/forseti_conf_server.yaml" \
     --log_level=${LOG_LEVEL} \
