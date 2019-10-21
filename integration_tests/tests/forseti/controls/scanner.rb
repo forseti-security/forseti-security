ea56949833050f857a9b6598548f784823e7c61a
Rehana Devani <rdevani@google.com>
2019-10-21 11:27:49 -0400

    Restoring from backup file

--- integration_tests/tests/forseti/controls/scanner.rb
+++ integration_tests/tests/forseti/controls/scanner.rb
@@ -15,18 +15,22 @@
 require 'json'
 control 'Scanner' do
 
-   describe "Run scanners" do
+
+   describe "Run scanner" do
 
        before :context do
            command("forseti inventory purge 0").result
            command("forseti inventory create").result
            inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
-           command("forseti model create --inventory_index_id #{inventory_id} model_scanner").result
-           command("forseti model use model_scanner").result
-           @unmodified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
+           command("forseti model create --inventory_index_id #{inventory_id} model_new").result
+           command("forseti model use model_new").result
+           command("sudo cp /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server_backup.yaml").result
            @modified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
            @modified_yaml["scanner"]["scanners"][0]["enabled"] = "true"
-           command("echo -en #{@modified_yaml} |  tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
+           puts "Trying again output", command("echo -en \"#{@modified_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").stdout
+           puts "Trying again error", command("echo -en \"#{@modified_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").stderr
+           command("echo -en \"#{@modified_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
+           puts "file content", command("cat /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").stdout
            command("forseti server configuration reload").result
        end
 
@@ -34,16 +38,11 @@ control 'Scanner' do
                expect(command("forseti scanner run").stdout).to match /audit/
            end
 
-           it "should be visible from the command-line" do
-               expect(command("forseti scanner run").stdout).to match /scan completed/
-           end
-
-
        after :context do
-           command("echo #{@unmodified_yaml} | tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
+           command("sudo mv /home/ubuntu/forseti-security/configs/forseti_conf_server_backup.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
            command("forseti server configuration reload").result
            command("forseti inventory purge 0").result
-           command("forseti model delete model_scanner").result
+           command("forseti model delete model_new").result
        end
    end
 end
