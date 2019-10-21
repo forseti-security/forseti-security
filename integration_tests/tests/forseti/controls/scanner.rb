8673e44395a31c1c6ad887c62b16d35e5d487e4c
Rehana Devani <rdevani@google.com>
2019-10-21 13:04:56 -0400

    Updated assertion

--- integration_tests/tests/forseti/controls/scanner.rb
+++ integration_tests/tests/forseti/controls/scanner.rb
@@ -21,18 +21,19 @@ control 'Scanner' do
        before :context do
            command("forseti inventory purge 0").result
            command("forseti inventory create").result
-           inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
-           command("forseti model create --inventory_index_id #{inventory_id} model_new").result
-           command("forseti model use model_new").result
+           @inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
+           command("forseti model create --inventory_index_id #{@inventory_id} model_scanner").result
+           command("forseti model use model_scanner").result
            command("sudo cp /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server_backup.yaml").result
            @modified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
            @modified_yaml["scanner"]["scanners"][0]["enabled"] = "true"
            command("echo -en \"#{@modified_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
+           puts "file content", command("cat /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").stdout
            command("forseti server configuration reload").result
        end
 
            it "should be visible from the command-line" do
-               expect(command("forseti scanner run").stdout).to match /audit/
+               expect(command("forseti scanner run").stdout).to match /AuditLoggingScanner/
            end
 
            it "should be visible from the command-line" do
@@ -43,7 +44,7 @@ control 'Scanner' do
            command("sudo mv /home/ubuntu/forseti-security/configs/forseti_conf_server_backup.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
            command("forseti server configuration reload").result
            command("forseti inventory purge 0").result
-           command("forseti model delete model_new").result
+           command("forseti model delete model_scanner").result
        end
    end
 end
