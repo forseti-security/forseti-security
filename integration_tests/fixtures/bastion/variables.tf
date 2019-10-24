7df9b73b143d46f6ad89d750f0c78f86e1885033
Darrell Kuhn <dkuhn@google.com>
2019-10-24 10:05:43 -0700

    Update variables.tf

--- integration_tests/fixtures/bastion/variables.tf
+++ integration_tests/fixtures/bastion/variables.tf
@@ -24,6 +24,12 @@ variable "project_id" {
   type        = "string"
 }
 
+variable "bastion_firewall_netblocks" {
+  description = "The trusted Travis CI firewall network blocks for bastion host ssh access. https://docs.travis-ci.com/user/ip-addresses/"
+  type        = string
+  default     = "34.66.25.221/32 34.66.50.208/32 34.66.178.120/32 34.66.200.49/32 34.68.144.114/32 35.184.226.236/32 35.188.1.99/32 35.188.15.155/32 35.188.73.34/32 35.192.10.37/32 35.192.85.2/32 35.192.91.101/32 35.192.136.167/32 35.192.187.174/32 35.193.7.13/32 35.193.14.140/32 35.202.145.110/32 35.202.245.105/32 35.222.7.205/32 35.224.112.202/32 104.154.113.151/32 104.154.120.187/32 104.197.122.201/32 104.198.131.58/32"
+}
+   
 variable "subnetwork" {
   description = "The name of the subnetwork in which the bastion host will be deployed."
   type        = "string"
