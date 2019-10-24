830557e6e531a98a179b6405dfb1635f9de0b65b
Darrell Kuhn <dkuhn@google.com>
2019-10-24 10:19:24 -0700

    Update variables.tf

--- integration_tests/fixtures/bastion/variables.tf
+++ integration_tests/fixtures/bastion/variables.tf
@@ -29,7 +29,7 @@ variable "bastion_firewall_netblocks" {
                  Travis netblock details here: https://docs.travis-ci.com/user/ip-addresses/.
                  Example shell command to build this list dynamically from DNS: 
                  dig +short nat.gce-us-central1.travisci.net '{print $0"/32"}' | tr '\n' ' '"
-  type        = string
+  type        = list
   default     = "34.66.25.221/32 34.66.50.208/32 34.66.178.120/32 34.66.200.49/32 34.68.144.114/32 35.184.226.236/32 35.188.1.99/32 35.188.15.155/32 35.188.73.34/32 35.192.10.37/32 35.192.85.2/32 35.192.91.101/32 35.192.136.167/32 35.192.187.174/32 35.193.7.13/32 35.193.14.140/32 35.202.145.110/32 35.202.245.105/32 35.222.7.205/32 35.224.112.202/32 104.154.113.151/32 104.154.120.187/32 104.197.122.201/32 104.198.131.58/32"
 }
    
