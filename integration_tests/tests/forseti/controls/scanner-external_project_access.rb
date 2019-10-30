7c6c7fc25f833516f2103797a1113cacf8d0e8e6
Va Ha <goofiva@gmail.com>
2019-10-29 22:14:01 -0700

    Converting to straight inspect.

--- integration_tests/tests/forseti/controls/scanner-external_project_access.rb
+++ integration_tests/tests/forseti/controls/scanner-external_project_access.rb
@@ -12,27 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-kms_resources_names = attribute('kms_resources_names')
-
 require 'json'
 control "scanner - external project access" do
-  before :context do
-    command("forseti inventory purge 0").result
-    command("forseti inventory create").result
-    inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
-    command("forseti model create --inventory_index_id #{inventory_id} model_new").result
-    command("forseti model use model_new").result
+  describe command("forseti inventory purge 0") do
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("forseti inventory create") do
+    its('exit_status') { should eq 0 }
+  end
+
+  inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
+
+  describe command("forseti model create --inventory_index_id #{inventory_id} model_new") do
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("forseti model use model_new") do
+    its('exit_status') { should eq 0 }
   end
 
   describe command("forseti scanner run --scanner external_project_access_scanner") do
     its('exit_status') { should eq 0 }
-    its('output') { should match (/Scanner Index ID: [0-9]* is created/)}
-    its('output') { should match (/Running ExternalProjectAccessScanner.../)}
-    its('output') { should match (/Scan completed!/)}
+    its('stdout') { should match (/Scanner Index ID: [0-9]* is created/)}
+    its('stdout') { should match (/Running ExternalProjectAccessScanner.../)}
+    its('stdout') { should match (/Scan completed!/)}
   end
 
-  after(:context) do
-    command("forseti inventory purge 0").result
-    command("forseti model delete model_new").result
+  describe command("forseti inventory purge 0") do
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("forseti model delete model_new") do
+    its('exit_status') { should eq 0 }
   end
 end
