1ff2a78cfe2bbaea0ef6923d90d03509c1e96c84
Gregg Kowalski <gkowalski@google.com>
2019-11-08 15:37:05 -0800

    Real commit this time

--- integration_tests/tests/forseti/controls/scanner-enable_audit_logging_scanner.rb
+++ integration_tests/tests/forseti/controls/scanner-enable_audit_logging_scanner.rb
@@ -15,22 +15,25 @@
 require 'json'
 require 'securerandom'
 
-random_string = SecureRandom.uuid.gsub!('-', '')
+model_name = SecureRandom.uuid.gsub!('-', '')
 
 control 'scanner - enable audit logging scanner' do
-  @inventory_id = /\"id\"\: \"([0-9]*)\"/.match(command("forseti inventory create --import_as #{random_string}").stdout)[1]
+  @inventory_id = /\"id\"\: \"([0-9]*)\"/.match(command("forseti inventory create --import_as #{model_name}").stdout)[1]
 
-  describe command("forseti model use #{random_string}") do
+  describe command("forseti model use #{model_name}") do
     its('exit_status') { should eq 0 }
   end
 
   # update server config to enable audit logging scanner
-  command("sudo cp /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server_backup.yaml").result
   @modified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
   @scanner_index = @modified_yaml["scanner"]["scanners"].find_index { |scanner| scanner["name"] == "audit_logging" }
-  @modified_yaml["scanner"]["scanners"][@scanner_index]["enabled"] = "true"
-  command("echo -en \"#{@modified_yaml.to_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
-  command("forseti server configuration reload").result
+  @modified_yaml["scanner"]["scanners"][@scanner_index]["enabled"] = true
+  describe command("echo -en \"#{@modified_yaml.to_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml") do
+    its('exit_status') { should eq 0 }
+  end
+  describe command("forseti server configuration reload") do
+    its('exit_status') { should eq 0 }
+  end
 
   # act
   describe command("forseti scanner run") do
@@ -40,7 +43,8 @@ control 'scanner - enable audit logging scanner' do
   end
 
   # disable audit logging scanner
-  describe command("sudo mv /home/ubuntu/forseti-security/configs/forseti_conf_server_backup.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml") do
+  @modified_yaml["scanner"]["scanners"][@scanner_index]["enabled"] = false
+  describe command("echo -en \"#{@modified_yaml.to_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml") do
     its('exit_status') { should eq 0 }
   end
   describe command("forseti server configuration reload").result do
@@ -51,7 +55,7 @@ control 'scanner - enable audit logging scanner' do
     its('exit_status') { should eq 0 }
   end
 
-  describe command("forseti model delete #{random_string}") do
+  describe command("forseti model delete #{model_name}") do
     its('exit_status') { should eq 0 }
   end
 end
