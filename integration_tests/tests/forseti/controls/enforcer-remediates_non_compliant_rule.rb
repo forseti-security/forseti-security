2bf19d2a701ad91fede6c2d8f36eae300c14f8d7
Va Ha <goofiva@gmail.com>
2019-11-10 19:40:24 -0800

    Added enforcer remediates non compliant rule test.

---
+++ integration_tests/tests/forseti/controls/enforcer-remediates_non_compliant_rule.rb
@@ -0,0 +1,74 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'json'
+
+project_id = attribute('project_id')
+random_string = SecureRandom.uuid.gsub!('-', '')
+
+control "enforcer - remediates non compliant rule" do
+  fw_rules = JSON.parse(command("gcloud compute firewall-rules list --format=json").stdout)
+
+  new_rules = []
+  fw_rules.each { |rule|
+    if  /forseti-test-allow-icmp-deleteme-/.match(rule["name"])
+      # mark all rules generated for this test for deletion
+      next
+    end
+    # Remove all the attributes enforcer does not like
+    rule.delete("kind")
+    rule.delete("selfLink")
+    rule.delete("id")
+    rule.delete("creationTimestamp")
+    rule.delete("targetServiceAccounts")
+    rule.delete("sourceServiceAccounts")
+    new_rules.push(rule)
+  }
+
+  # Write the current firewall rules to a json file
+  describe command("echo '#{JSON.dump(new_rules)}' > /tmp/current_fw_rule.json") do
+    its('exit_status') { should eq 0 }
+  end
+
+  # Make sure the file exist
+  describe file('/tmp/current_fw_rule.json') do
+    it { should exist }
+  end
+
+  # Create a rule for deletion
+  describe command("gcloud compute firewall-rules create forseti-test-allow-icmp-deleteme-#{random_string} --source-ranges=10.0.0.0/32 --description=\"Allow ICMP from private subnet\" --allow=icmp") do
+    its('exit_status') { should eq 0 }
+  end
+
+  # Sync the rules with enforcer
+  describe command("forseti_enforcer --enforce_project #{project_id} --policy_file /tmp/current_fw_rule.json") do
+    its('exit_status') { should eq 0 }
+  end
+
+  # Make sure rule no longer exist
+  describe command("gcloud compute firewall-rules list --format=json") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not match (/forseti-test-allow-icmp-deleteme-/)}
+  end
+
+  # Delete the rule file
+  describe command("rm /tmp/current_fw_rule.json") do
+    its('exit_status') { should eq 0 }
+  end
+
+  # Make sure the file has been deleted
+  describe file('/tmp/current_fw_rule.json') do
+    it { should_not exist }
+  end
+end
