72157a334f547c98549c11d048ed0fc3acdd3968
Va Ha <goofiva@gmail.com>
2019-11-14 17:04:57 -0800

    Revert "Using forseti-cai-storage-bucket attribute for gs name."
    This reverts commit 3a541486b4496cea97ab11b00bcfa066c58266ea.

--- integration_tests/tests/forseti/controls/inventory-cai_gcs_export_file.rb
+++ integration_tests/tests/forseti/controls/inventory-cai_gcs_export_file.rb
@@ -18,12 +18,12 @@ require 'json'
 suffix = attribute('suffix')
 org_id = attribute('org_id')
 
-random_string = SecureRandom.uuid.gsub!('-', '')[0..10]
+random_string = SecureRandom.uuid.gsub!('-', '')
 
 control "inventory - cai gcs export file" do
   @inventory_id = /\"id\"\: \"([0-9]*)\"/.match(command("forseti inventory create --import_as #{random_string}").stdout)[1]
 
-  gs_file = "gs://#{forseti_cai_storage_bucket}/organizations-#{org_id}-resource-#{@inventory_id}.dump"
+  gs_file = "gs://forseti-cai-export-#{suffix}/organizations-#{org_id}-resource-#{@inventory_id}.dump"
 
   describe command("gsutil ls #{gs_file} | grep #{gs_file}") do
     its('stdout') { should match (gs_file)}
