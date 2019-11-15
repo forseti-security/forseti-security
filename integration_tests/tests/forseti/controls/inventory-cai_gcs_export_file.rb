3a8a9fa8ed979549f25b65d8891eb0c089409bd9
Va Ha <goofiva@gmail.com>
2019-11-14 16:56:06 -0800

    Using cai storage bucket attribute.

--- integration_tests/tests/forseti/controls/inventory-cai_gcs_export_file.rb
+++ integration_tests/tests/forseti/controls/inventory-cai_gcs_export_file.rb
@@ -15,16 +15,17 @@
 require 'securerandom'
 require 'json'
 
-suffix = attribute('suffix')
 org_id = attribute('org_id')
+forseti_cai_storage_bucket = attribute('forseti-cai-storage-bucket')
 
 random_string = SecureRandom.uuid.gsub!('-', '')[0..10]
 
 control "inventory - cai gcs export file" do
   @inventory_id = /\"id\"\: \"([0-9]*)\"/.match(command("forseti inventory create --import_as #{random_string}").stdout)[1]
-  gs_file = "gs://#{forseti-cai-storage-bucket}/organizations-#{org_id}-resource-#{@inventory_id}.dump"
+  gs_file = "gs://#{forseti_cai_storage_bucket}/organizations-#{org_id}-resource-#{@inventory_id}.dump"
 
   describe command("gsutil ls #{gs_file} | grep #{gs_file}") do
+    its('exit_status') { should eq 0 }
     its('stdout') { should match (gs_file)}
   end
 
