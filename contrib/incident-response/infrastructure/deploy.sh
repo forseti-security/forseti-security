1e9cfc03a69ffd120d0a6daad1b4e02508f19f51
Johan Berggren <jberggren@gmail.com>
2019-12-09 23:26:57 +0100

    Deploy script

---
+++ contrib/incident-response/infrastructure/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+terraform init
+terraform apply -var gcp_project=$DEVSHELL_PROJECT_ID -auto-approve
+
+# Turbinia
+cd ~
+virtualenv --python=/usr/bin/python2.7 turbinia
+source turbinia/bin/activate
+pip install turbinia
+cd $DIR
+terraform output turbinia-config > ~/.turbiniarc
+sed -i s/"\/var\/log\/turbinia\/turbinia.log"/"\/tmp\/turbinia.log"/ ~/.turbiniarc
+
+echo "Deployment done!"
+terraform output
+
