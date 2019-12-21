804504f8c0680a59e122c27b2c154c1693dcfe5e
Va Ha <goofiva@gmail.com>
2019-12-20 16:28:48 -0800

    WIP inventory performance test.

--- integration_tests/tests/forseti/controls/inventory-performance.rb
+++ integration_tests/tests/forseti/controls/inventory-performance.rb
@@ -20,15 +20,20 @@ cai_dump_file_gcs_paths = attribute('inventory-performance-cai-dump-paths')
 db_password = attribute('forseti-cloudsql-password')
 db_user_name = attribute('forseti-cloudsql-user')
 
-control "inventory-get-list" do
+conf_path = "/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml"
+
+control "inventory-performance" do
   # Arrange
   # Update forseti config: disable api polling, fake org id, gcs paths
   # Make backup of server config
-  describe command("cp /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml /tmp/forseti_conf_server_backup.yaml") do
+  describe command("cp #{conf_path} /tmp/forseti_conf_server_backup.yaml") do
     its('exit_status') { should eq 0 }
   end
 
-  @modified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
+  @modified_yaml = yaml(conf_path).params
+  describe command("echo -en \"#{@modified_yaml.to_yaml}\"") do
+    its('exit_status') { should eq 0 }
+  end
   # fake org id
   @modified_yaml["inventory"]["root_resource_id"] = "organizations/5456546415"
   # disable_polling: set to True for all found
@@ -36,9 +41,9 @@ control "inventory-get-list" do
     @modified_yaml["inventory"]["api_quota"][key]["disable_polling"] = true
   end
   # under cai: section: cai_dump_file_gcs_paths: ['gs://forseti-server-XXXXX/testing-iam-policy.dump','gs://forseti-server-XXXX/testing-resource.dump']
-  @modified_yaml["cai"]["section"]["cai_dump_file_gcs_paths"] = cai_dump_file_gcs_paths
+  @modified_yaml["inventory"]["cai"]["cai_dump_file_gcs_paths"] = cai_dump_file_gcs_paths
 
-  describe command("echo -en \"#{@modified_yaml.to_yaml}\" | sudo tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml") do
+  describe command("echo -en \"#{@modified_yaml.to_yaml}\" | sudo tee #{conf_path}") do
     its('exit_status') { should eq 0 }
   end
   describe command("forseti server configuration reload") do
@@ -54,13 +59,10 @@ control "inventory-get-list" do
   end
 
   # Act
-  inventory_create = command("forseti inventory create")
-  describe(inventory_create) do
+  describe command("forseti inventory create") do
     its('exit_status') { should eq 0 }
     its('stdout') { should match (/\"id\"\: \"([0-9]*)\"/)}
   end
-  @inventory_id = /\"id\"\: \"([0-9]*)\"/.match(inventory_create.stdout)[1]
-
 
   # Assert
   # Assert Inventory takes less than 12 minutes
@@ -68,6 +70,7 @@ control "inventory-get-list" do
   # Compare completeTimestamp and startTimestamp, it should not be more than 12 mins apart
   describe command("mysql -u #{db_user_name} -p#{db_password} --host 127.0.0.1 --database forseti_security --execute \"SELECT COUNT(*) FROM inventory_index where id=#{@inventory_id}\"") do
     its('exit_status') { should eq 0 }
+    its('stdout') { should match (/lets_see_what_it_says/)}
   end
 
   # Assert resource count
@@ -92,7 +95,7 @@ control "inventory-get-list" do
   end
 
   # Restore server config backup
-  describe command("cp /tmp/forseti_conf_server_backup.yaml /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml") do
+  describe command("sudo cp /tmp/forseti_conf_server_backup.yaml #{conf_path}") do
     its('exit_status') { should eq 0 }
   end
   describe command("forseti server configuration reload") do
@@ -107,10 +110,6 @@ control "inventory-get-list" do
 
   # Cleanup
   describe command("forseti inventory delete #{@inventory_id}") do
-    its('exit_status') { should eq 0 }
-  end
-
-  describe command("forseti model delete #{model_name}") do
     its('exit_status') { should eq 0 }
   end
 end
