2e1feecbcff7e47c674bdfc2d18aca6e2b855ba5
Gregg Kowalski <gkowalski@google.com>
2020-01-24 00:00:04 -0800

    Revert makefile changes. Update forseti test fixture to always destroy buckets to fix tf destroy error.

--- integration_tests/fixtures/forseti/modules/test_resources/main.tf
+++ integration_tests/fixtures/forseti/modules/test_resources/main.tf
@@ -1,5 +1,5 @@
 /**
-* Copyright 2019 Google LLC
+* Copyright 2020 Google LLC
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -32,9 +32,10 @@ resource "google_compute_firewall" "enforcer_allow_all_icmp_rule" {
 # inventory-cai_enabled_vs_disabled.rb: Create a bucket in the EU
 #-------------------------#
 resource "google_storage_bucket" "inventory_cai_eu" {
-  name     = "foresti-test-bucket-eu-${var.random_test_id}"
-  project  = var.project_id
-  location = "EU"
+  name          = "foresti-test-bucket-eu-${var.random_test_id}"
+  project       = var.project_id
+  location      = "EU"
+  force_destroy = true
 }
 
 resource "google_storage_bucket_access_control" "inventory_cai_eu_acl" {
@@ -49,9 +50,10 @@ resource "google_storage_bucket_access_control" "inventory_cai_eu_acl" {
 # scanner-bucket_acl_scanner.rb: Create a bucket with AllAuth + All Users Reader ACL
 #-------------------------#
 resource "google_storage_bucket" "bucket_acl_scanner" {
-  name     = "foresti-test-bucket-${var.random_test_id}"
-  project  = var.project_id
-  location = "US"
+  name          = "foresti-test-bucket-${var.random_test_id}"
+  project       = var.project_id
+  location      = "US"
+  force_destroy = true
 }
 
 resource "google_storage_bucket_access_control" "bucket_acl_scanner_all_auth_acl" {
