24d478ef1aa11033c8c7215944e03c2e5b8f3407
Rehana Devani <rdevani@google.com>
2020-01-29 11:46:26 -0500

    Restart Forseti to release used memory

--- install/gcp/scripts/run_forseti.sh
+++ install/gcp/scripts/run_forseti.sh
@@ -94,3 +94,7 @@ echo "Finished running Forseti notifier."
 # Clean up the model tables
 echo "Cleaning up model tables"
 forseti model delete ${MODEL_NAME}
+
+# Restart Forseti to release used memory
+systemctl restart forseti
+
