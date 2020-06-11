818ff97fd57490def687534dee4ff69f4a15584e
Johan Berggren <jberggren@gmail.com>
2020-06-11 07:59:24 +0200

    Wait for TS

--- contrib/incident-response/infrastructure/deploy.sh
+++ contrib/incident-response/infrastructure/deploy.sh
@@ -14,10 +14,33 @@ terraform apply -var gcp_project=$DEVSHELL_PROJECT_ID -auto-approve
 cd ~
 virtualenv --python=/usr/bin/python2.7 turbinia
 source turbinia/bin/activate
-pip install turbinia
+
+pip install turbinia 1>/dev/null
 cd $DIR
 terraform output turbinia-config > ~/.turbiniarc
 sed -i s/"\/var\/log\/turbinia\/turbinia.log"/"\/tmp\/turbinia.log"/ ~/.turbiniarc
 
-echo "Deployment done!"
-terraform output
+url="$(terraform output timesketch-server-url)"
+user="$(terraform output timesketch-admin-username)"
+pass="$(terraform output timesketch-admin-password)"
+
+echo
+echo "Waiting for Timesketch installation to finish. This may take a few minutes.."
+echo
+while true; do
+  response="$(curl -k -o /dev/null --silent --head --write-out '%{http_code}' $url)"
+  if [[ "${response}" -eq "302" ]]; then
+    break
+  fi
+  sleep 3
+done
+
+echo "****************************************************************************"
+echo "Timesketch server: ${url}"
+echo "User: ${user}"
+echo "Password: ${pass}"
+echo "****************************************************************************"
+
+echo
+echo "Deployment done"
+echo
